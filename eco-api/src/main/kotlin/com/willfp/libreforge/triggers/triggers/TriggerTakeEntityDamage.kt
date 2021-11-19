c544d4600b20e46ef6d014e4c062a0bf9cba6102
Auxilor <william.favierparsons1@gmail.com>
2021-11-19 19:24:07 +0000

    Added TriggerTakeEntityDamage and EffectRunCommand

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/triggers/triggers/TriggerTakeEntityDamage.kt
@@ -0,0 +1,55 @@
+package com.willfp.libreforge.triggers.triggers
+
+import com.willfp.eco.core.integrations.antigrief.AntigriefManager
+import com.willfp.eco.core.integrations.mcmmo.McmmoManager
+import com.willfp.libreforge.triggers.Trigger
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.wrappers.WrappedDamageEvent
+import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityDamageEvent
+
+class TriggerTakeEntityDamage : Trigger("take_entity_damage") {
+    @EventHandler(ignoreCancelled = true)
+    fun onMeleeAttack(event: EntityDamageByEntityEvent) {
+        if (McmmoManager.isFake(event)) {
+            return
+        }
+
+        val attacker = event.damager
+
+        if (attacker !is LivingEntity) {
+            return
+        }
+
+        val victim = event.entity
+
+        if (victim !is Player) {
+            return
+        }
+
+        if (event.isCancelled) {
+            return
+        }
+
+        if (event.cause == EntityDamageEvent.DamageCause.THORNS) {
+            return
+        }
+
+        if (!AntigriefManager.canInjure(victim, attacker)) {
+            return
+        }
+
+        this.processTrigger(
+            victim,
+            TriggerData(
+                player = victim,
+                victim = attacker,
+                location = attacker.location,
+                event = WrappedDamageEvent(event)
+            )
+        )
+    }
+}
