8480d0902bf37026abbf85a12332e2c3d08b84f4
Auxilor <william.favierparsons1@gmail.com>
2021-11-23 13:03:23 +0000

    WrappedEvent.kt changes

--- eco-api/src/main/kotlin/com/willfp/libreforge/triggers/wrappers/WrappedHungerEvent.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/triggers/wrappers/WrappedHungerEvent.kt
@@ -1,11 +1,11 @@
 package com.willfp.libreforge.triggers.wrappers
 
-import com.willfp.libreforge.triggers.WrappedEvent
+import com.willfp.libreforge.triggers.WrappedCancellableEvent
 import org.bukkit.event.entity.FoodLevelChangeEvent
 
 class WrappedHungerEvent(
     private val event: FoodLevelChangeEvent
-) : WrappedEvent<FoodLevelChangeEvent> {
+) : WrappedCancellableEvent<FoodLevelChangeEvent> {
     var amount: Int
         get() {
             return event.entity.foodLevel - event.foodLevel
@@ -14,7 +14,7 @@ class WrappedHungerEvent(
             event.foodLevel = event.entity.foodLevel - value
         }
 
-    var isCancelled: Boolean
+    override var isCancelled: Boolean
         get() {
             return event.isCancelled
         }
