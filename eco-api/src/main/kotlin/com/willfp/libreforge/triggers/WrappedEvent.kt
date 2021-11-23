32832ac12571fd159d210b291eca1cc0f44b4391
Auxilor <william.favierparsons1@gmail.com>
2021-11-23 13:03:23 +0000

    WrappedEvent.kt changes

--- eco-api/src/main/kotlin/com/willfp/libreforge/triggers/WrappedEvent.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/triggers/WrappedEvent.kt
@@ -1,5 +1,10 @@
 package com.willfp.libreforge.triggers
 
+import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
 
 interface WrappedEvent<out T : Event>
+
+interface WrappedCancellableEvent<out T> : WrappedEvent<T> where T: Event, T: Cancellable {
+    var isCancelled: Boolean
+}
