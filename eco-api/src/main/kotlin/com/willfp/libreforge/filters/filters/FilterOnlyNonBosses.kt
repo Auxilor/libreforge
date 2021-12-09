c1160ee9309c55eb7cd15ff95fb6c131ea67b88d
Auxilor <william.favierparsons1@gmail.com>
2021-12-09 15:50:26 +0000

    Added OnlyNonBosses

--- eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterOnlyNonBosses.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterOnlyNonBosses.kt
@@ -8,17 +8,17 @@ import org.bukkit.entity.Boss
 import org.bukkit.entity.ElderGuardian
 import org.bukkit.persistence.PersistentDataType
 
-class FilterOnlyBosses: FilterComponent() {
+class FilterOnlyNonBosses: FilterComponent() {
     override fun passes(data: TriggerData, config: Config): Boolean {
         val entity = data.victim ?: return true
 
-        val onlyBosses = config.getBoolOrNull("onlyBosses") ?: false
+        val onlyNonBosses = config.getBoolOrNull("onlyNonBosses") ?: false
 
-        if (!onlyBosses) {
+        if (!onlyNonBosses) {
             return true
         }
 
-        return entity is Boss || entity is ElderGuardian || entity.persistentDataContainer
+        return entity !is Boss && entity !is ElderGuardian && !entity.persistentDataContainer
             .has(NamespacedKeyUtils.create("ecobosses", "boss"), PersistentDataType.STRING)
     }
 }
