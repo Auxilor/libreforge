e5eb524bf01448a9182b449b3a53e677fb05f8d9
Auxilor <william.favierparsons1@gmail.com>
2021-12-23 17:25:29 +0000

    Moved movement condition listeners to MovementConditionListener.kt

--- eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionInWater.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionInWater.kt
@@ -3,27 +3,9 @@ package com.willfp.libreforge.conditions.conditions
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.libreforge.ConfigViolation
 import com.willfp.libreforge.conditions.Condition
-import com.willfp.libreforge.updateEffects
 import org.bukkit.entity.Player
-import org.bukkit.event.EventHandler
-import org.bukkit.event.EventPriority
-import org.bukkit.event.player.PlayerMoveEvent
 
 class ConditionInWater: Condition("in_water") {
-    @EventHandler(
-        priority = EventPriority.MONITOR,
-        ignoreCancelled = true
-    )
-    fun handle(event: PlayerMoveEvent) {
-        val player = event.player
-
-        if (event.from.world?.getBlockAt(event.from)?.type == event.to.world?.getBlockAt(event.to)?.type) {
-            return
-        }
-
-        player.updateEffects()
-    }
-
     override fun isConditionMet(player: Player, config: Config): Boolean {
         return player.isInWater == config.getBool("in_water")
     }
