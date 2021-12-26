ccafe681f55569208a64278187f72ea19acdf9a8
Auxilor <william.favierparsons1@gmail.com>
2021-12-26 12:59:57 +0000

    Improved FilterEntityType

--- eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterEntityType.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterEntityType.kt
@@ -2,15 +2,17 @@ package com.willfp.libreforge.filters.filters
 
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.libreforge.filters.FilterComponent
+import com.willfp.libreforge.filters.containsIgnoreCase
 import com.willfp.libreforge.triggers.TriggerData
 
-class FilterEntityType: FilterComponent() {
+class FilterEntityType : FilterComponent() {
     override fun passes(data: TriggerData, config: Config): Boolean {
         val entity = data.victim ?: return true
 
-        val entityNames = config.getStringsOrNull("entities", false)
-            ?.map { it.lowercase() } ?: emptyList()
 
-        return entityNames.contains(entity.type.name.lowercase()) || entityNames.contains(entity.category.name.lowercase())
+        val entityNames = config.getStrings("entities", false)
+
+        return entityNames.containsIgnoreCase(entity.type.name)
+                || entityNames.containsIgnoreCase(entity.category.name)
     }
 }
