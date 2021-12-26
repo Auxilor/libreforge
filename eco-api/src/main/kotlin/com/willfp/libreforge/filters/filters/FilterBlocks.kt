dab42314ae6bf7fbf0962a2783503ddba4b3ec9f
Auxilor <william.favierparsons1@gmail.com>
2021-12-26 12:55:45 +0000

    Internal changes for readability

--- eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterBlocks.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterBlocks.kt
@@ -2,15 +2,14 @@ package com.willfp.libreforge.filters.filters
 
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.libreforge.filters.FilterComponent
+import com.willfp.libreforge.filters.containsIgnoreCase
 import com.willfp.libreforge.triggers.TriggerData
 
-class FilterBlocks: FilterComponent() {
+class FilterBlocks : FilterComponent() {
     override fun passes(data: TriggerData, config: Config): Boolean {
         val block = data.block ?: return true
 
-        val materialNames = config.getStringsOrNull("blocks", false)
-            ?.map { it.uppercase() } ?: emptyList()
-
-        return materialNames.contains(block.type.name)
+        return config.getStringsOrNull("blocks", false)
+            ?.containsIgnoreCase(block.type.name) ?: true
     }
 }
--- eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterDamageCause.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterDamageCause.kt
@@ -2,15 +2,14 @@ package com.willfp.libreforge.filters.filters
 
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.libreforge.filters.FilterComponent
+import com.willfp.libreforge.filters.containsIgnoreCase
 import com.willfp.libreforge.triggers.TriggerData
 
 class FilterDamageCause: FilterComponent() {
     override fun passes(data: TriggerData, config: Config): Boolean {
         val cause = data.damageCause ?: return true
 
-        val causeNames = config.getStringsOrNull("damageCause", false)
-            ?.map { it.uppercase() } ?: emptyList()
-
-        return causeNames.contains(cause.name)
+        return config.getStringsOrNull("damageCause", false)
+            ?.containsIgnoreCase(cause.name) ?: true
     }
 }
