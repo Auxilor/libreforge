92177420233c053a7b91fa9c18b2ade37cf8b274
Auxilor <william.favierparsons1@gmail.com>
2021-12-26 13:07:03 +0000

    Added TriggerGainSkillXp

--- eco-api/src/main/kotlin/com/willfp/libreforge/integrations/ecoskills/EcoSkillsIntegration.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/ecoskills/EcoSkillsIntegration.kt
@@ -3,18 +3,21 @@ package com.willfp.libreforge.integrations.ecoskills
 import com.willfp.eco.core.integrations.Integration
 import com.willfp.libreforge.conditions.Condition
 import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.triggers.Trigger
 
 object EcoSkillsIntegration : Integration {
     private lateinit var ADD_STAT: Effect
     private lateinit var MULTIPLY_STAT: Effect
     private lateinit var HAS_SKILL_LEVEL: Condition
     private lateinit var SKILL_XP_MULTIPLIER: Effect
+    private lateinit var GAIN_SKILL_XP: Trigger
 
     fun load() {
         ADD_STAT = EffectAddStat()
         MULTIPLY_STAT = EffectMultiplyStat()
         HAS_SKILL_LEVEL = ConditionHasSkillLevel()
         SKILL_XP_MULTIPLIER = EffectSkillXpMultiplier()
+        GAIN_SKILL_XP = TriggerGainSkillXp()
     }
 
     override fun getPluginName(): String {
---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/ecoskills/TriggerGainSkillXp.kt
@@ -0,0 +1,32 @@
+package com.willfp.libreforge.integrations.ecoskills
+
+import com.willfp.eco.core.integrations.mcmmo.McmmoManager
+import com.willfp.ecoskills.api.PlayerSkillExpGainEvent
+import com.willfp.libreforge.triggers.Trigger
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.event.EventHandler
+
+class TriggerGainSkillXp : Trigger(
+    "gain_skill_xp", listOf(
+        TriggerParameter.PLAYER,
+        TriggerParameter.EVENT
+    )
+) {
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: PlayerSkillExpGainEvent) {
+        if (McmmoManager.isFake(event)) {
+            return
+        }
+
+        val player = event.player
+
+        this.processTrigger(
+            player,
+            TriggerData(
+                player = player,
+                event = WrappedSkillXpEvent(event)
+            )
+        )
+    }
+}
