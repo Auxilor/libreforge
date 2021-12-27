8f441be21f532705971705cd06747362bda0f837
Auxilor <william.favierparsons1@gmail.com>
2021-12-27 19:33:41 +0000

    Added food_multiplier

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectFoodMultiplier.kt
@@ -0,0 +1,86 @@
+package com.willfp.libreforge.effects.effects
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.core.integrations.mcmmo.McmmoManager
+import com.willfp.eco.util.NumberUtils
+import com.willfp.libreforge.ConfigViolation
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.effects.MultiplierModifier
+import com.willfp.libreforge.effects.getEffectAmount
+import com.willfp.libreforge.triggers.wrappers.WrappedHungerEvent
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.FoodLevelChangeEvent
+import java.util.*
+import kotlin.math.ceil
+
+class EffectFoodMultiplier : Effect("food_multiplier") {
+    private val modifiers = mutableMapOf<UUID, MutableList<MultiplierModifier>>()
+
+    override fun handleEnable(player: Player, config: Config) {
+        val registeredModifiers = modifiers[player.uniqueId] ?: mutableListOf()
+        val uuid = this.getUUID(player.getEffectAmount(this))
+        registeredModifiers.removeIf { it.uuid == uuid }
+        registeredModifiers.add(
+            MultiplierModifier(
+                uuid,
+                config.getDouble("multiplier")
+            )
+        )
+        modifiers[player.uniqueId] = registeredModifiers
+    }
+
+    override fun handleDisable(player: Player) {
+        val registeredModifiers = modifiers[player.uniqueId] ?: mutableListOf()
+        val uuid = this.getUUID(player.getEffectAmount(this))
+        registeredModifiers.removeIf { it.uuid == uuid }
+        modifiers[player.uniqueId] = registeredModifiers
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: FoodLevelChangeEvent) {
+        if (McmmoManager.isFake(event)) {
+            return
+        }
+
+        val player = event.entity
+
+        if (player !is Player) {
+            return
+        }
+
+        var multiplier = 1.0
+
+        for (modifier in (modifiers[player.uniqueId] ?: emptyList())) {
+            multiplier *= modifier.multiplier
+        }
+
+        val wrapped = WrappedHungerEvent(event)
+
+        if (wrapped.amount < 0) {
+            return
+        }
+
+        if (multiplier < 1) {
+            if (NumberUtils.randFloat(0.0, 1.0) > multiplier) {
+                wrapped.isCancelled = true
+            }
+        } else {
+            wrapped.amount = ceil(wrapped.amount * multiplier).toInt()
+        }
+    }
+
+    override fun validateConfig(config: Config): List<ConfigViolation> {
+        val violations = mutableListOf<ConfigViolation>()
+
+        config.getDoubleOrNull("multiplier")
+            ?: violations.add(
+                ConfigViolation(
+                    "multiplier",
+                    "You must specify the food multiplier!"
+                )
+            )
+
+        return violations
+    }
+}
