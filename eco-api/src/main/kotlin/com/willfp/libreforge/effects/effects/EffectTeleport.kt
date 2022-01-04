848cbfac3c9bc866e6f00aa9a539b7df2e81a227
Auxilor <william.favierparsons1@gmail.com>
2022-01-04 12:12:21 +0000

    Added teleport effect

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectTeleport.kt
@@ -0,0 +1,21 @@
+package com.willfp.libreforge.effects.effects
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import com.willfp.libreforge.triggers.Triggers
+
+class EffectTeleport : Effect(
+    "teleport",
+    supportsFilters = true,
+    applicableTriggers = Triggers.withParameters(
+        TriggerParameter.PLAYER,
+        TriggerParameter.LOCATION
+    )
+) {
+    override fun handle(data: TriggerData, config: Config) {
+        val player = data.player ?: return
+        player.teleport(data.location ?: return)
+    }
+}
