2fbc4acadbd03aa8b739bdb8a119281ac9f355f2
Auxilor <william.favierparsons1@gmail.com>
2022-01-10 10:25:40 +0000

    Added give_food effect

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectGiveFood.kt
@@ -0,0 +1,36 @@
+package com.willfp.libreforge.effects.effects
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.ConfigViolation
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import com.willfp.libreforge.triggers.Triggers
+
+class EffectGiveFood : Effect(
+    "give_food",
+    supportsFilters = true,
+    applicableTriggers = Triggers.withParameters(
+        TriggerParameter.PLAYER
+    )
+) {
+    override fun handle(data: TriggerData, config: Config) {
+        val player = data.player ?: return
+
+        player.foodLevel = player.foodLevel + config.getInt("amount")
+    }
+
+    override fun validateConfig(config: Config): List<ConfigViolation> {
+        val violations = mutableListOf<ConfigViolation>()
+
+        config.getIntOrNull("amount")
+            ?: violations.add(
+                ConfigViolation(
+                    "amount",
+                    "You must specify the amount of food to give!"
+                )
+            )
+
+        return violations
+    }
+}
