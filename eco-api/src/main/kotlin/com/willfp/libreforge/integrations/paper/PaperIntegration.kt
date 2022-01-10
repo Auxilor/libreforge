3a4e32009becd2bd6e51d8060644b32c6e102e3b
Auxilor <william.favierparsons1@gmail.com>
2022-01-10 16:44:00 +0000

    Fixed swing trigger on 1.16.5

--- eco-api/src/main/kotlin/com/willfp/libreforge/integrations/paper/PaperIntegration.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/paper/PaperIntegration.kt
@@ -1,11 +1,14 @@
 package com.willfp.libreforge.integrations.paper
 
+import com.willfp.eco.core.Prerequisite
 import com.willfp.libreforge.triggers.Trigger
 
 object PaperIntegration {
     private lateinit var SWING: Trigger
 
     fun load() {
-        SWING = TriggerSwing()
+        if (Prerequisite.HAS_1_17.isMet) {
+            SWING = TriggerSwing()
+        }
     }
 }
