c06db7bfedee29064af43953d32bc7a77cf33d86
Auxilor <william.favierparsons1@gmail.com>
2022-01-10 10:26:21 +0000

    Added give_health effect

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectGiveHealth.kt
@@ -0,0 +1,36 @@
+package com.willfp.libreforge.effects.effects
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.ConfigViolation
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import com.willfp.libreforge.triggers.Triggers
+
+class EffectGiveHealth : Effect(
+    "give_health",
+    supportsFilters = true,
+    applicableTriggers = Triggers.withParameters(
+        TriggerParameter.PLAYER
+    )
+) {
+    override fun handle(data: TriggerData, config: Config) {
+        val player = data.player ?: return
+
+        player.health = player.health + config.getDouble("amount")
+    }
+
+    override fun validateConfig(config: Config): List<ConfigViolation> {
+        val violations = mutableListOf<ConfigViolation>()
+
+        config.getDoubleOrNull("amount")
+            ?: violations.add(
+                ConfigViolation(
+                    "amount",
+                    "You must specify the amount of health to give!"
+                )
+            )
+
+        return violations
+    }
+}
