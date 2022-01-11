555ccd5908cae06d50b31f94b291b29af671b045
Auxilor <william.favierparsons1@gmail.com>
2022-01-11 09:18:00 +0000

    Refined swing trigger test

--- eco-api/src/main/kotlin/com/willfp/libreforge/integrations/paper/PaperIntegration.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/paper/PaperIntegration.kt
@@ -1,13 +1,13 @@
 package com.willfp.libreforge.integrations.paper
 
-import com.willfp.eco.core.Prerequisite
+import com.willfp.eco.util.ClassUtils
 import com.willfp.libreforge.triggers.Trigger
 
 object PaperIntegration {
     private lateinit var SWING: Trigger
 
     fun load() {
-        if (Prerequisite.HAS_1_17.isMet) {
+        if (ClassUtils.exists("io.papermc.paper.event.player.PlayerArmSwingEvent")) {
             SWING = TriggerSwing()
         }
     }
