558dd8142348335e540d6a66db2fa2b66cc688fd
0ft3n <dkoften@gmail.com>
2022-01-12 16:22:13 +0300

    Updated EffectIgnite.kt to the latest Libreforge

--- eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectIgnite.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectIgnite.kt
@@ -1,6 +1,7 @@
 package com.willfp.libreforge.effects.effects
 
 import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.ConfigViolation
 import com.willfp.libreforge.effects.Effect
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
@@ -13,14 +14,15 @@ class EffectIgnite: Effect(
     "ignite",
     true,
     Triggers.withParameters(
-        TriggerParameter.VICTIM
+        TriggerParameter.VICTIM,
+        TriggerParameter.PLAYER
     )
 ) {
 
     override fun handle(data: TriggerData, config: Config) {
         val victim = data.victim ?: return
-        val damage = config.getDouble("damage-per-tick")
-        val duration = config.getInt("ticks")
+        val damage = config.getDoubleFromExpression("damage-per-tick", data.player)
+        val duration = config.getIntFromExpression("ticks", data.player)
 
         victim.fireTicks = duration
         victim.setMetadata("ignitedMob", FixedMetadataValue(plugin, damage))
@@ -31,6 +33,23 @@ class EffectIgnite: Effect(
         if (event.cause != EntityDamageEvent.DamageCause.FIRE_TICK) return
         if (!event.entity.hasMetadata("ignitedMob")) return
         event.damage = event.entity.getMetadata("ignitedMob")[0].asDouble()
+    }
+
+    override fun validateConfig(config: Config): List<ConfigViolation> {
+        val violations = mutableListOf<ConfigViolation>()
+        if (!config.has("damage-per-tick")) violations.add(
+            ConfigViolation(
+                "damage-per-tick",
+                "You must specify the damage of fire per tick!"
+            )
+        )
+        if (!config.has("ticks")) violations.add(
+            ConfigViolation(
+                "ticks",
+                "You must specify the duration of igniting in ticks!"
+            )
+        )
+        return violations
     }
 
 }
