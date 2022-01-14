a53dffd3d33fde2f22946920a420d8aca2872c8f
Auxilor <william.favierparsons1@gmail.com>
2022-01-14 15:31:29 +0000

    Added entity lookups to FilterEntityType

--- eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterEntityType.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterEntityType.kt
@@ -1,6 +1,7 @@
 package com.willfp.libreforge.filters.filters
 
 import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.core.entities.Entities
 import com.willfp.libreforge.filters.FilterComponent
 import com.willfp.libreforge.filters.containsIgnoreCase
 import com.willfp.libreforge.triggers.TriggerData
@@ -10,8 +11,10 @@ class FilterEntityType : FilterComponent() {
         val entity = data.victim ?: return true
 
         val entityNames = config.getStrings("entities")
+        val testables = entityNames.map { Entities.lookup(it) }
 
         return entityNames.containsIgnoreCase(entity.type.name)
                 || entityNames.containsIgnoreCase(entity.category.name)
+                || testables.stream().anyMatch { it.matches(entity) }
     }
 }
