f2f21de8acfc3cef94858ab3f4aaa630a9c5e38b
Auxilor <william.favierparsons1@gmail.com>
2022-01-19 19:07:56 +0000

    Added mine_block_progress trigger

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/triggers/triggers/TriggerMineBlockProgress.kt
@@ -0,0 +1,35 @@
+package com.willfp.libreforge.triggers.triggers
+
+import com.willfp.eco.core.integrations.mcmmo.McmmoManager
+import com.willfp.libreforge.triggers.Trigger
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.event.EventHandler
+import org.bukkit.event.block.BlockDamageEvent
+
+class TriggerMineBlockProgress : Trigger(
+    "mine_block_progress", listOf(
+        TriggerParameter.PLAYER,
+        TriggerParameter.BLOCK,
+        TriggerParameter.LOCATION
+    )
+) {
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: BlockDamageEvent) {
+        if (McmmoManager.isFake(event)) {
+            return
+        }
+
+        val player = event.player
+        val block = event.block
+
+        this.processTrigger(
+            player,
+            TriggerData(
+                player = player,
+                block = block,
+                location = block.location
+            )
+        )
+    }
+}
