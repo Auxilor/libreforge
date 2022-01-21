16a9f8c55d82612929970cd656776fff92177ee3
Auxilor <william.favierparsons1@gmail.com>
2022-01-21 08:58:43 +0000

    Added multiply_drops effect

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectMultiplyDrops.kt
@@ -0,0 +1,53 @@
+package com.willfp.libreforge.effects.effects
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.ConfigViolation
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.filters.containsIgnoreCase
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import com.willfp.libreforge.triggers.Triggers
+import com.willfp.libreforge.triggers.wrappers.WrappedDropEvent
+import kotlin.math.roundToInt
+
+class EffectMultiplyDrops : Effect(
+    "multiply_drops",
+    supportsFilters = true,
+    applicableTriggers = Triggers.withParameters(
+        TriggerParameter.EVENT
+    )
+) {
+    override fun handle(data: TriggerData, config: Config) {
+        val event = data.event as? WrappedDropEvent<*> ?: return
+
+        event.modifier = {
+            val fortune = config.getIntFromExpression("fortune")
+
+            if (fortune > 0 && it.maxStackSize > 1 && config.getStrings("on_items").containsIgnoreCase(it.type.name)) {
+                it.amount = (Math.random() * (fortune.toDouble() - 1) + 1.1).roundToInt()
+            }
+
+            Pair(it, 0)
+        }
+    }
+
+    override fun validateConfig(config: Config): List<ConfigViolation> {
+        val violations = mutableListOf<ConfigViolation>()
+
+        if (!config.has("fortune")) violations.add(
+            ConfigViolation(
+                "fortune",
+                "You must specify the fortune to give!"
+            )
+        )
+
+        if (!config.has("on_items")) violations.add(
+            ConfigViolation(
+                "on_items",
+                "You must specify the items that can be multiplied!"
+            )
+        )
+
+        return violations
+    }
+}
