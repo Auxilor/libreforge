5bc636b605cb65349e49d8bc0d2968ad63dc145d
Auxilor <william.favierparsons1@gmail.com>
2022-01-22 11:22:46 +0000

    Added spawn_particle effect

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectSpawnParticle.kt
@@ -0,0 +1,45 @@
+package com.willfp.libreforge.effects.effects
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.ConfigViolation
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import com.willfp.libreforge.triggers.Triggers
+import org.bukkit.Particle
+
+class EffectSpawnParticle : Effect(
+    "spawn_particle",
+    supportsFilters = true,
+    applicableTriggers = Triggers.withParameters(
+        TriggerParameter.LOCATION
+    )
+) {
+    override fun handle(data: TriggerData, config: Config) {
+        val location = data.location ?: return
+        val world = location.world ?: return
+        val particle = Particle.valueOf(config.getString("particle").uppercase())
+        val amount = config.getIntFromExpression("amount")
+        world.spawnParticle(particle, location, amount)
+    }
+
+    override fun validateConfig(config: Config): List<ConfigViolation> {
+        val violations = mutableListOf<ConfigViolation>()
+
+        if (!config.has("particle")) violations.add(
+            ConfigViolation(
+                "particle",
+                "You must specify the particle!"
+            )
+        )
+
+        if (!config.has("amount")) violations.add(
+            ConfigViolation(
+                "amount",
+                "You must specify the amount of particles to spawn!"
+            )
+        )
+
+        return violations
+    }
+}
