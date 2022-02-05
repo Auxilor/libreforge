49a7e611d33ece01aeebfbc4f3cd50b2234323d8
Auxilor <william.favierparsons1@gmail.com>
2022-02-05 12:04:06 +0000

    Added effect chains and the run_chain effect

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/chains/EffectChain.kt
@@ -0,0 +1,15 @@
+package com.willfp.libreforge.chains
+
+import com.willfp.libreforge.effects.ConfiguredEffect
+import com.willfp.libreforge.triggers.InvocationData
+
+class EffectChain(
+    val id: String,
+    val effects: Iterable<ConfiguredEffect>
+) {
+    operator fun invoke(invocationData: InvocationData) {
+        for (effect in effects) {
+            effect(invocationData)
+        }
+    }
+}
--- eco-api/src/main/kotlin/com/willfp/libreforge/triggers/Trigger.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/triggers/Trigger.kt
@@ -1,18 +1,11 @@
 package com.willfp.libreforge.triggers
 
-import com.willfp.eco.core.integrations.antigrief.AntigriefManager
-import com.willfp.eco.core.integrations.economy.EconomyManager
-import com.willfp.eco.util.NumberUtils
 import com.willfp.libreforge.Holder
 import com.willfp.libreforge.LibReforgePlugin
-import com.willfp.libreforge.events.EffectActivateEvent
 import com.willfp.libreforge.getHolders
 import org.bukkit.entity.Player
 import org.bukkit.event.Listener
 import java.util.Objects
-import java.util.UUID
-
-private val everyHandler = mutableMapOf<UUID, MutableMap<UUID, Int>>()
 
 abstract class Trigger(
     val id: String,
@@ -41,81 +34,8 @@ abstract class Trigger(
                 continue
             }
 
-            for ((effect, config, filter, triggers, uuid, conditions) in holder.effects) {
-                var effectAreMet = true
-                for ((condition, conditionConfig) in conditions) {
-                    if (!condition.isConditionMet(player, conditionConfig)) {
-                        effectAreMet = false
-                    }
-                }
-
-                if (!effectAreMet) {
-                    continue
-                }
-
-                if (NumberUtils.randFloat(0.0, 100.0) > (config.getDoubleOrNull("chance") ?: 100.0)) {
-                    continue
-                }
-
-                if (config.has("check_antigrief") && config.getBool("check_antigrief") && data.player != null && data.victim != null) {
-                    if (!AntigriefManager.canInjure(data.player, data.victim)) {
-                        continue
-                    }
-                }
-
-                val every = config.getIntOrNull("every") ?: 0
-
-                if (every > 0) {
-                    val everyMap = everyHandler[player.uniqueId] ?: mutableMapOf()
-                    var current = everyMap[uuid] ?: 0
-
-                    if (current != 0) {
-                        current++
-
-                        if (current >= every) {
-                            current = 0
-                        }
-
-                        everyHandler[player.uniqueId] = everyMap.apply {
-                            this[uuid] = current
-                        }
-
-                        continue
-                    }
-                }
-
-                if (!triggers.contains(this)) {
-                    continue
-                }
-
-                if (!filter.matches(data)) {
-                    continue
-                }
-
-                if (effect.getCooldown(player, uuid) > 0) {
-                    if (config.getBoolOrNull("send_cooldown_message") != false) {
-                        effect.sendCooldownMessage(player, uuid)
-                    }
-                    continue
-                }
-
-                if (config.has("cost")) {
-                    if (!EconomyManager.hasAmount(player, config.getDoubleFromExpression("cost"))) {
-                        effect.sendCannotAffordMessage(player, config.getDoubleFromExpression("cost"))
-                        continue
-                    }
-
-                    EconomyManager.removeMoney(player, config.getDoubleFromExpression("cost"))
-                }
-
-                val activateEvent = EffectActivateEvent(player, holder, effect, config)
-                this.plugin.server.pluginManager.callEvent(activateEvent)
-
-                if (!activateEvent.isCancelled) {
-                    effect.resetCooldown(player, config, uuid)
-
-                    effect.handle(data, config)
-                }
+            for (effect in holder.effects) {
+                effect(InvocationData(player, data, holder, this))
             }
         }
     }
@@ -132,3 +52,10 @@ abstract class Trigger(
         return Objects.hash(id)
     }
 }
+
+data class InvocationData(
+    val player: Player,
+    val data: TriggerData,
+    val holder: Holder,
+    val trigger: Trigger
+)
