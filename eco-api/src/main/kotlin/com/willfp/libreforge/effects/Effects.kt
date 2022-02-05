76879270b1a6554f40f353411e05cd706c329a48
Auxilor <william.favierparsons1@gmail.com>
2022-02-05 12:57:27 +0000

    Registered run_chain effect

--- eco-api/src/main/kotlin/com/willfp/libreforge/effects/Effects.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/Effects.kt
@@ -6,12 +6,57 @@ import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.libreforge.ConfigViolation
 import com.willfp.libreforge.LibReforgePlugin
 import com.willfp.libreforge.conditions.Conditions
-import com.willfp.libreforge.effects.effects.*
+import com.willfp.libreforge.effects.effects.EffectArmor
+import com.willfp.libreforge.effects.effects.EffectArmorToughness
+import com.willfp.libreforge.effects.effects.EffectArrowRing
+import com.willfp.libreforge.effects.effects.EffectAttackSpeedMultiplier
+import com.willfp.libreforge.effects.effects.EffectAutosmelt
+import com.willfp.libreforge.effects.effects.EffectBleed
+import com.willfp.libreforge.effects.effects.EffectBonusHealth
+import com.willfp.libreforge.effects.effects.EffectBreakBlock
+import com.willfp.libreforge.effects.effects.EffectCancelEvent
+import com.willfp.libreforge.effects.effects.EffectCritMultiplier
+import com.willfp.libreforge.effects.effects.EffectDamageArmor
+import com.willfp.libreforge.effects.effects.EffectDamageMultiplier
+import com.willfp.libreforge.effects.effects.EffectDamageNearbyEntities
+import com.willfp.libreforge.effects.effects.EffectDrill
+import com.willfp.libreforge.effects.effects.EffectExtinguish
+import com.willfp.libreforge.effects.effects.EffectFeatherStep
+import com.willfp.libreforge.effects.effects.EffectFoodMultiplier
+import com.willfp.libreforge.effects.effects.EffectGiveFood
+import com.willfp.libreforge.effects.effects.EffectGiveHealth
+import com.willfp.libreforge.effects.effects.EffectGiveMoney
+import com.willfp.libreforge.effects.effects.EffectGiveOxygen
+import com.willfp.libreforge.effects.effects.EffectGivePoints
+import com.willfp.libreforge.effects.effects.EffectGiveXp
+import com.willfp.libreforge.effects.effects.EffectHungerMultiplier
+import com.willfp.libreforge.effects.effects.EffectIgnite
+import com.willfp.libreforge.effects.effects.EffectKnockbackMultiplier
+import com.willfp.libreforge.effects.effects.EffectMineRadius
+import com.willfp.libreforge.effects.effects.EffectMovementSpeedMultiplier
+import com.willfp.libreforge.effects.effects.EffectMultiplyDrops
+import com.willfp.libreforge.effects.effects.EffectMultiplyPoints
+import com.willfp.libreforge.effects.effects.EffectPermanentPotionEffect
+import com.willfp.libreforge.effects.effects.EffectPlaySound
+import com.willfp.libreforge.effects.effects.EffectPotionEffect
+import com.willfp.libreforge.effects.effects.EffectPullToLocation
+import com.willfp.libreforge.effects.effects.EffectRegenMultiplier
+import com.willfp.libreforge.effects.effects.EffectRemovePotionEffect
+import com.willfp.libreforge.effects.effects.EffectRunChain
+import com.willfp.libreforge.effects.effects.EffectRunCommand
+import com.willfp.libreforge.effects.effects.EffectRunPlayerCommand
+import com.willfp.libreforge.effects.effects.EffectSendMessage
+import com.willfp.libreforge.effects.effects.EffectSetPoints
+import com.willfp.libreforge.effects.effects.EffectSpawnMobs
+import com.willfp.libreforge.effects.effects.EffectSpawnParticle
+import com.willfp.libreforge.effects.effects.EffectStrikeLightning
+import com.willfp.libreforge.effects.effects.EffectTeleport
+import com.willfp.libreforge.effects.effects.EffectXpMultiplier
 import com.willfp.libreforge.filters.ConfiguredFilter
 import com.willfp.libreforge.filters.EmptyFilter
 import com.willfp.libreforge.triggers.Trigger
 import com.willfp.libreforge.triggers.Triggers
-import java.util.*
+import java.util.UUID
 
 @Suppress("UNUSED")
 object Effects {
@@ -63,6 +108,7 @@ object Effects {
     val DRILL: Effect = EffectDrill()
     val DAMAGE_NEARBY_ENTITIES: Effect = EffectDamageNearbyEntities()
     val SEND_TITLE: Effect = EffectDamageNearbyEntities()
+    val RUN_CHAIN: Effect = EffectRunChain()
 
     /**
      * Get effect matching id.
