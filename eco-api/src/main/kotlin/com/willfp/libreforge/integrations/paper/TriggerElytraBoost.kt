312bc5bbe2bd45cf4d2dda2df735ee08bfc03af3
Auxilor <william.favierparsons1@gmail.com>
2022-03-08 20:16:25 +0000

    Added elytra_boost

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/paper/TriggerElytraBoost.kt
@@ -0,0 +1,36 @@
+package com.willfp.libreforge.integrations.paper
+
+import com.destroystokyo.paper.event.player.PlayerElytraBoostEvent
+import com.willfp.eco.core.integrations.mcmmo.McmmoManager
+import com.willfp.libreforge.triggers.Trigger
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.event.EventHandler
+
+class TriggerElytraBoost : Trigger(
+    "elytra_boost", listOf(
+        TriggerParameter.PLAYER,
+        TriggerParameter.LOCATION
+    )
+) {
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: PlayerElytraBoostEvent) {
+        if (McmmoManager.isFake(event)) {
+            return
+        }
+
+        val player = event.player
+
+        if (event.isCancelled) {
+            return
+        }
+
+        this.processTrigger(
+            player,
+            TriggerData(
+                player = player,
+                location = player.location
+            )
+        )
+    }
+}
