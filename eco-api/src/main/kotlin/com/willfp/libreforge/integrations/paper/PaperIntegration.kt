0eacf2b1f46971cb6fa72aff8b0647c917ecdadf
Auxilor <william.favierparsons1@gmail.com>
2022-03-08 20:16:25 +0000

    Added elytra_boost

--- eco-api/src/main/kotlin/com/willfp/libreforge/integrations/paper/PaperIntegration.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/paper/PaperIntegration.kt
@@ -5,10 +5,13 @@ import com.willfp.libreforge.triggers.Trigger
 
 object PaperIntegration {
     private lateinit var SWING: Trigger
+    private lateinit var ELYTRA_BOOST: Trigger
 
     fun load() {
         if (ClassUtils.exists("io.papermc.paper.event.player.PlayerArmSwingEvent")) {
             SWING = TriggerSwing()
         }
+
+        ELYTRA_BOOST = TriggerElytraBoost()
     }
 }
