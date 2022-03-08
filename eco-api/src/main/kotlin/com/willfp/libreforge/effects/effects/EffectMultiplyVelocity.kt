ceeaff678ac263f7e4423e8ad1e6e5e3f35a7d6c
Auxilor <william.favierparsons1@gmail.com>
2022-03-08 20:18:01 +0000

    Added multiply_velocity effect

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectMultiplyVelocity.kt
@@ -0,0 +1,33 @@
+package com.willfp.libreforge.effects.effects
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.ConfigViolation
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import com.willfp.libreforge.triggers.Triggers
+
+class EffectMultiplyVelocity : Effect(
+    "multiply_velocity",
+    applicableTriggers = Triggers.withParameters(
+        TriggerParameter.PLAYER
+    )
+) {
+    override fun handle(data: TriggerData, config: Config) {
+        val player = data.player ?: return
+        player.velocity = player.velocity.multiply(config.getDoubleFromExpression("multiplier", player))
+    }
+
+    override fun validateConfig(config: Config): List<ConfigViolation> {
+        val violations = mutableListOf<ConfigViolation>()
+
+        if (!config.has("multiplier")) violations.add(
+            ConfigViolation(
+                "multiplier",
+                "You must specify the velocity multiplier!"
+            )
+        )
+
+        return violations
+    }
+}
