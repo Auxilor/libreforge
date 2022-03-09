1662026a9aa97915312692798d22ada0e323f9ca
Auxilor <william.favierparsons1@gmail.com>
2022-03-09 20:30:03 +0000

    Added shoot_arrow effect, shoot_bow trigger, and WrappedShootBowEvent

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectShootArrow.kt
@@ -0,0 +1,42 @@
+package com.willfp.libreforge.effects.effects
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.ConfigViolation
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.runExempted
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import com.willfp.libreforge.triggers.Triggers
+import com.willfp.libreforge.triggers.wrappers.WrappedShootBowEvent
+import org.bukkit.entity.AbstractArrow
+import org.bukkit.entity.Arrow
+
+class EffectShootArrow : Effect(
+    "shoot_arrow",
+    applicableTriggers = Triggers.withParameters(
+        TriggerParameter.PLAYER
+    )
+) {
+    override fun handle(data: TriggerData, config: Config) {
+        val player = data.player ?: return
+        val velocity = (data.event as? WrappedShootBowEvent)?.velocity
+        val fire = (data.event as? WrappedShootBowEvent)?.hasFire
+
+        player.runExempted {
+            val arrow = if (velocity == null) {
+                it.launchProjectile(Arrow::class.java)
+            } else {
+                it.launchProjectile(Arrow::class.java, velocity)
+            }
+
+            arrow.pickupStatus = AbstractArrow.PickupStatus.DISALLOWED
+            if (fire == true) {
+                arrow.fireTicks = Int.MAX_VALUE
+            }
+        }
+    }
+
+    override fun validateConfig(config: Config): List<ConfigViolation> {
+        return emptyList()
+    }
+}
---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/triggers/triggers/TriggerShootBow.kt
@@ -0,0 +1,41 @@
+package com.willfp.libreforge.triggers.triggers
+
+import com.willfp.eco.core.integrations.mcmmo.McmmoManager
+import com.willfp.libreforge.triggers.Trigger
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import com.willfp.libreforge.triggers.wrappers.WrappedShootBowEvent
+import org.bukkit.entity.Player
+import org.bukkit.entity.Projectile
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityShootBowEvent
+
+class TriggerShootBow : Trigger(
+    "shoot_bow", listOf(
+        TriggerParameter.PLAYER,
+        TriggerParameter.PROJECTILE,
+        TriggerParameter.EVENT
+    )
+) {
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: EntityShootBowEvent) {
+        if (McmmoManager.isFake(event)) {
+            return
+        }
+
+        val shooter = event.entity
+
+        if (shooter !is Player) {
+            return
+        }
+
+        this.processTrigger(
+            shooter,
+            TriggerData(
+                player = shooter,
+                projectile = event.projectile as? Projectile,
+                event = WrappedShootBowEvent(event)
+            )
+        )
+    }
+}
