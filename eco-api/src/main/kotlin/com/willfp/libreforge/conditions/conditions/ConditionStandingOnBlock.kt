d019b8873ce14f8b338c15083ce5e73ed02530e3
Auxilor <william.favierparsons1@gmail.com>
2022-03-12 21:11:47 +0000

    Fixed ConditionStandingOnBlock and ConditionIsGliding

--- eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionStandingOnBlock.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionStandingOnBlock.kt
@@ -5,7 +5,7 @@ import com.willfp.libreforge.ConfigViolation
 import com.willfp.libreforge.conditions.Condition
 import org.bukkit.entity.Player
 
-class ConditionStandingOnBlock : Condition("is_gliding") {
+class ConditionStandingOnBlock : Condition("standing_on_block") {
     override fun isConditionMet(player: Player, config: Config): Boolean {
         return player.world.getBlockAt(player.location.clone().add(0.0, -1.0, 0.0))
             .type.name.equals(config.getString("block"), ignoreCase = true)
