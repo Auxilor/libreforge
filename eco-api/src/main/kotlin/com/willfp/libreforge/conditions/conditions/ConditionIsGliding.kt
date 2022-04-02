c8904f25423a1673f487c833d489f5e556306f49
Auxilor <william.favierparsons1@gmail.com>
2022-04-02 13:58:12 +0100

    Stopped requiring explicit options for inverse-able conditions

--- eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionIsGliding.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionIsGliding.kt
@@ -1,25 +1,11 @@
 package com.willfp.libreforge.conditions.conditions
 
 import com.willfp.eco.core.config.interfaces.Config
-import com.willfp.libreforge.ConfigViolation
 import com.willfp.libreforge.conditions.Condition
 import org.bukkit.entity.Player
 
 class ConditionIsGliding : Condition("is_gliding") {
     override fun isConditionMet(player: Player, config: Config): Boolean {
-        return player.isGliding == config.getBool("is_gliding")
-    }
-
-    override fun validateConfig(config: Config): List<ConfigViolation> {
-        val violations = mutableListOf<ConfigViolation>()
-
-        if (!config.has("is_gliding")) violations.add(
-            ConfigViolation(
-                "is_gliding",
-                "You must specify if the player must be gliding or not!"
-            )
-        )
-
-        return violations
+        return player.isGliding == (config.getBoolOrNull("is_gliding") ?: true)
     }
 }
--- eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionIsNight.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionIsNight.kt
@@ -1,7 +1,6 @@
 package com.willfp.libreforge.conditions.conditions
 
 import com.willfp.eco.core.config.interfaces.Config
-import com.willfp.libreforge.ConfigViolation
 import com.willfp.libreforge.conditions.Condition
 import org.bukkit.entity.Player
 
@@ -9,19 +8,6 @@ class ConditionIsNight : Condition("is_night") {
     override fun isConditionMet(player: Player, config: Config): Boolean {
         val isNight = player.world.time < 12300 || player.world.time > 23850
 
-        return isNight == config.getBool("is_night")
-    }
-
-    override fun validateConfig(config: Config): List<ConfigViolation> {
-        val violations = mutableListOf<ConfigViolation>()
-
-        if (!config.has("is_night")) violations.add(
-            ConfigViolation(
-                "is_night",
-                "You must specify if the time must be night or not!"
-            )
-        )
-
-        return violations
+        return isNight == (config.getBoolOrNull("is_night") ?: true)
     }
 }
