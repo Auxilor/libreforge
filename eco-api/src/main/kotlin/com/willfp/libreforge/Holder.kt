6c2a141d6465ce22a2ce452c1b54a343cd4b0855
Auxilor <william.favierparsons1@gmail.com>
2022-05-05 21:17:25 +0100

    Added holder ID

--- eco-api/src/main/kotlin/com/willfp/libreforge/Holder.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/Holder.kt
@@ -4,6 +4,8 @@ import com.willfp.libreforge.conditions.ConfiguredCondition
 import com.willfp.libreforge.effects.ConfiguredEffect
 
 interface Holder {
+    val id: String?
+        get() = null
     val effects: Set<ConfiguredEffect>
     val conditions: Set<ConfiguredCondition>
 }
