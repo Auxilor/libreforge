9a3f44c3071194c1afc34a7f71e319fc849d65c4
Auxilor <william.favierparsons1@gmail.com>
2022-05-08 16:04:40 +0100

    Added not met lines to holder

--- eco-api/src/main/kotlin/com/willfp/libreforge/Holder.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/Holder.kt
@@ -2,9 +2,22 @@ package com.willfp.libreforge
 
 import com.willfp.libreforge.conditions.ConfiguredCondition
 import com.willfp.libreforge.effects.ConfiguredEffect
+import org.bukkit.entity.Player
 
 interface Holder {
     val id: String
     val effects: Set<ConfiguredEffect>
     val conditions: Set<ConfiguredCondition>
+
+    fun getNotMetLines(player: Player): List<String> {
+        val lines = mutableListOf<String>()
+
+        for (condition in this.conditions) {
+            if (condition.isMet(player)) {
+                lines.addAll(condition.notMetLines ?: continue)
+            }
+        }
+
+        return lines
+    }
 }
