a337425bfbb45a511fc31342f0265abe1ea0c0c6
Auxilor <william.favierparsons1@gmail.com>
2022-05-17 18:50:27 +0100

    Optimized not met lines

--- eco-api/src/main/kotlin/com/willfp/libreforge/Holder.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/Holder.kt
@@ -1,8 +1,15 @@
 package com.willfp.libreforge
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import com.willfp.libreforge.conditions.ConfiguredCondition
 import com.willfp.libreforge.effects.ConfiguredEffect
 import org.bukkit.entity.Player
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+private val lineCache = Caffeine.newBuilder()
+    .expireAfterWrite(1, TimeUnit.SECONDS)
+    .build<LineCacheEntry, List<String>>()
 
 interface Holder {
     val id: String
@@ -10,14 +17,22 @@ interface Holder {
     val conditions: Set<ConfiguredCondition>
 
     fun getNotMetLines(player: Player): List<String> {
-        val lines = mutableListOf<String>()
+        return lineCache.get(LineCacheEntry(player.uniqueId, this.id)) {
+            val lines = mutableListOf<String>()
 
-        for (condition in this.conditions) {
-            if (!condition.isMet(player)) {
-                lines.addAll(condition.notMetLines ?: continue)
+            for (condition in this.conditions) {
+                if (!condition.isMet(player)) {
+                    lines.addAll(condition.notMetLines ?: continue)
+                }
             }
-        }
 
-        return lines
+            lines
+        }
     }
 }
+
+
+private data class LineCacheEntry(
+    val uuid: UUID,
+    val holderID: String
+)
