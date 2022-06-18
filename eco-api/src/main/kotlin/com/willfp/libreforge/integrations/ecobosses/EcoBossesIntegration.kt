e9b84039e88e1a923cc89c687fe506c6d2148774
Auxilor <william.favierparsons1@gmail.com>
2022-06-18 16:57:20 +0100

    Added kill_boss and spawn_boss triggers

--- eco-api/src/main/kotlin/com/willfp/libreforge/integrations/ecobosses/EcoBossesIntegration.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/ecobosses/EcoBossesIntegration.kt
@@ -1,11 +1,16 @@
 package com.willfp.libreforge.integrations.ecobosses
 
 import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.triggers.Trigger
 
 object EcoBossesIntegration {
     private lateinit var BOSS_DROP_CHANCE_MULTIPLIER: Effect
+    private lateinit var KILL_BOSS: Trigger
+    private lateinit var SPAWN_BOSS: Trigger
 
     fun load() {
         BOSS_DROP_CHANCE_MULTIPLIER = EffectBossDropChanceMultiplier()
+        KILL_BOSS = TriggerKillBoss()
+        SPAWN_BOSS = TriggerSpawnBoss()
     }
 }
