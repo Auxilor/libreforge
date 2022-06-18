04976253cc8d4604b5611148f137e982922cc54e
Auxilor <william.favierparsons1@gmail.com>
2022-06-18 16:49:34 +0100

    Added wearing_boots, wearing_chestplate, wearing_helmet, and wearing_leggings

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionWearingChestplate.kt
@@ -0,0 +1,28 @@
+package com.willfp.libreforge.conditions.conditions
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.core.items.Items
+import com.willfp.libreforge.ConfigViolation
+import com.willfp.libreforge.conditions.Condition
+import org.bukkit.entity.Player
+
+class ConditionWearingChestplate : Condition("wearing_chestplate") {
+    override fun isConditionMet(player: Player, config: Config): Boolean {
+        return config.getStrings("items").map { Items.lookup(it) }.any {
+            it.matches(player.inventory.chestplate)
+        }
+    }
+
+    override fun validateConfig(config: Config): List<ConfigViolation> {
+        val violations = mutableListOf<ConfigViolation>()
+
+        if (!config.has("items")) violations.add(
+            ConfigViolation(
+                "items",
+                "You must specify the list of items!"
+            )
+        )
+
+        return violations
+    }
+}
