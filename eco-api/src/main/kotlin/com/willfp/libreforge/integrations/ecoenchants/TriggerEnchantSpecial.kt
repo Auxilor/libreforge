1c7219742d597999a16c07f16169219fc220adfb
Auxilor <william.favierparsons1@gmail.com>
2022-06-18 17:08:25 +0100

    Fixed enchant_special

--- eco-api/src/main/kotlin/com/willfp/libreforge/integrations/ecoenchants/TriggerEnchantSpecial.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/ecoenchants/TriggerEnchantSpecial.kt
@@ -5,7 +5,6 @@ import com.willfp.ecoenchants.enchantments.meta.EnchantmentType
 import com.willfp.libreforge.triggers.Trigger
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
-import org.bukkit.GameMode
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.enchantment.EnchantItemEvent
@@ -20,10 +19,6 @@ class TriggerEnchantSpecial : Trigger(
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     fun handleLevelling(event: EnchantItemEvent) {
         val player = event.enchanter
-
-        if (player.gameMode == GameMode.CREATIVE || player.gameMode == GameMode.SPECTATOR) {
-            return
-        }
 
         this.plugin.scheduler.runLater({
             if (EcoEnchants.hasAnyOfType(event.item, EnchantmentType.SPECIAL)) {
