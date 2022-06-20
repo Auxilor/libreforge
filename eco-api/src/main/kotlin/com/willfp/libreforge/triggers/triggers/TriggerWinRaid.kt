37bdd98f7a5fdbc8293ff00eaec4f80b8643f12c
Auxilor <william.favierparsons1@gmail.com>
2022-06-20 14:15:37 +0100

    Added win_raid trigger

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/triggers/triggers/TriggerWinRaid.kt
@@ -0,0 +1,33 @@
+package com.willfp.libreforge.triggers.triggers
+
+import com.willfp.eco.core.integrations.mcmmo.McmmoManager
+import com.willfp.libreforge.triggers.Trigger
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.event.EventHandler
+import org.bukkit.event.raid.RaidFinishEvent
+
+class TriggerWinRaid : Trigger(
+    "win_raid", listOf(
+        TriggerParameter.PLAYER,
+        TriggerParameter.LOCATION
+    )
+) {
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: RaidFinishEvent) {
+        if (McmmoManager.isFake(event)) {
+            return
+        }
+
+        for (player in event.winners) {
+            this.processTrigger(
+                player,
+                TriggerData(
+                    player = player,
+                    location = event.raid.location
+                ),
+                event.raid.badOmenLevel.toDouble() + 1
+            )
+        }
+    }
+}
