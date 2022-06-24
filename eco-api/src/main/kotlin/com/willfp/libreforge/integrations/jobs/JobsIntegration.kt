96a6020ff86c9b009de0b6e8fa4fcf8b1497d48d
Auxilor <william.favierparsons1@gmail.com>
2022-06-24 13:55:48 +0100

    Added jobs_level_up trigger

--- eco-api/src/main/kotlin/com/willfp/libreforge/integrations/jobs/JobsIntegration.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/jobs/JobsIntegration.kt
@@ -2,14 +2,17 @@ package com.willfp.libreforge.integrations.jobs
 
 import com.willfp.eco.core.integrations.Integration
 import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.triggers.Trigger
 
 object JobsIntegration : Integration {
     private lateinit var JOBS_XP_MULTIPLIER: Effect
     private lateinit var JOBS_MONEY_MULTIPLIER: Effect
+    private lateinit var JOBS_LEVEL_UP: Trigger
 
     fun load() {
         JOBS_XP_MULTIPLIER = EffectJobsXpMultiplier()
         JOBS_MONEY_MULTIPLIER = EffectJobsMoneyMultiplier()
+        JOBS_LEVEL_UP = TriggerJobsLevelUp()
     }
 
     override fun getPluginName(): String {
