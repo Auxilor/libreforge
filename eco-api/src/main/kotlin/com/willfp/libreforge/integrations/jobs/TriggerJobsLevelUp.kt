92022ec7c68dcd4bdd678fcbfcb1ddf858979173
Auxilor <william.favierparsons1@gmail.com>
2022-06-24 13:55:48 +0100

    Added jobs_level_up trigger

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/jobs/TriggerJobsLevelUp.kt
@@ -0,0 +1,33 @@
+package com.willfp.libreforge.integrations.jobs
+
+import com.gamingmesh.jobs.api.JobsLevelUpEvent
+import com.willfp.eco.core.integrations.mcmmo.McmmoManager
+import com.willfp.libreforge.triggers.Trigger
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.event.EventHandler
+
+class TriggerJobsLevelUp : Trigger(
+    "jobs_level_up", listOf(
+        TriggerParameter.PLAYER,
+        TriggerParameter.LOCATION
+    )
+) {
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: JobsLevelUpEvent) {
+        if (McmmoManager.isFake(event)) {
+            return
+        }
+
+        val player = event.player.player
+
+        this.processTrigger(
+            player,
+            TriggerData(
+                player = player,
+                location = player.location
+            ),
+            event.level.toDouble()
+        )
+    }
+}
