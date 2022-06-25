bf10310e4271372ce4016a989cdeb8fb09142581
Auxilor <william.favierparsons1@gmail.com>
2022-06-25 22:21:08 +0100

    Optimized in_mainhand, in_offhand, wearing_boots, wearing_chestplate, wearing_helmet, and wearing_leggings

--- eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionInOffhand.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionInOffhand.kt
@@ -2,15 +2,20 @@ package com.willfp.libreforge.conditions.conditions
 
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.eco.core.items.Items
+import com.willfp.eco.core.items.TestableItem
 import com.willfp.libreforge.ConfigViolation
 import com.willfp.libreforge.conditions.Condition
+import com.willfp.libreforge.effects.CompileData
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 
 class ConditionInOffhand : Condition("in_offhand") {
-    override fun isConditionMet(player: Player, config: Config): Boolean {
-        return config.getStrings("items").map { Items.lookup(it) }.any {
-            it.matches(player.inventory.itemInMainHand)
+    override fun isConditionMet(player: Player, config: Config, data: CompileData?): Boolean {
+        if (data !is ItemCompileData) {
+            return true
         }
+
+        return data.isMet(player.inventory.itemInOffHand)
     }
 
     override fun validateConfig(config: Config): List<ConfigViolation> {
@@ -24,5 +29,25 @@ class ConditionInOffhand : Condition("in_offhand") {
         )
 
         return violations
+    }
+
+    override fun makeCompileData(config: Config, context: String): CompileData {
+        return ItemCompileData(config.getStrings("items").map {
+            Items.lookup(it)
+        })
+    }
+
+    private class ItemCompileData(
+        override val data: Iterable<TestableItem>
+    ) : CompileData {
+        fun isMet(itemStack: ItemStack?): Boolean {
+            val list = data.toList()
+
+            if (list.isEmpty()) {
+                return true
+            }
+
+            return list.any { it.matches(itemStack) }
+        }
     }
 }
--- eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionWearingBoots.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionWearingBoots.kt
@@ -2,15 +2,20 @@ package com.willfp.libreforge.conditions.conditions
 
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.eco.core.items.Items
+import com.willfp.eco.core.items.TestableItem
 import com.willfp.libreforge.ConfigViolation
 import com.willfp.libreforge.conditions.Condition
+import com.willfp.libreforge.effects.CompileData
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 
 class ConditionWearingBoots : Condition("wearing_boots") {
-    override fun isConditionMet(player: Player, config: Config): Boolean {
-        return config.getStrings("items").map { Items.lookup(it) }.any {
-            it.matches(player.inventory.boots)
+    override fun isConditionMet(player: Player, config: Config, data: CompileData?): Boolean {
+        if (data !is ItemCompileData) {
+            return true
         }
+
+        return data.isMet(player.inventory.boots)
     }
 
     override fun validateConfig(config: Config): List<ConfigViolation> {
@@ -24,5 +29,25 @@ class ConditionWearingBoots : Condition("wearing_boots") {
         )
 
         return violations
+    }
+
+    override fun makeCompileData(config: Config, context: String): CompileData {
+        return ItemCompileData(config.getStrings("items").map {
+            Items.lookup(it)
+        })
+    }
+
+    private class ItemCompileData(
+        override val data: Iterable<TestableItem>
+    ) : CompileData {
+        fun isMet(itemStack: ItemStack?): Boolean {
+            val list = data.toList()
+
+            if (list.isEmpty()) {
+                return true
+            }
+
+            return list.any { it.matches(itemStack) }
+        }
     }
 }
