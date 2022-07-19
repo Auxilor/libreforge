a90a3e3c51bb736fc984124b904378fb8debc8ff
Auxilor <william.favierparsons1@gmail.com>
2022-07-20 00:07:43 +0100

    Added rotate effect

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectRotate.kt
@@ -0,0 +1,22 @@
+package com.willfp.libreforge.effects.effects
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import com.willfp.libreforge.triggers.Triggers
+
+class EffectRotate : Effect(
+    "rotate",
+    applicableTriggers = Triggers.withParameters(
+        TriggerParameter.PLAYER
+    )
+) {
+    override fun handle(data: TriggerData, config: Config) {
+        val player = data.player ?: return
+        val yaw = player.location.yaw + config.getDoubleFromExpression("angle", player).toFloat()
+
+
+        player.location.yaw = yaw % 360f
+    }
+}
