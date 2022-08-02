317dd19597f1a8da97f62142b97df8ed5c6b1c13
Auxilor <william.favierparsons1@gmail.com>
2022-08-02 16:14:10 +0100

    Improved effect collision fixer

--- eco-api/src/main/kotlin/com/willfp/libreforge/EffectCollisionFixer.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/EffectCollisionFixer.kt
@@ -1,11 +1,12 @@
 package com.willfp.libreforge
 
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerQuitEvent
 
 object EffectCollisionFixer : Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     fun clearOnQuit(event: PlayerQuitEvent) {
         val player = event.player
         for (effect in player.getActiveEffects()) {
