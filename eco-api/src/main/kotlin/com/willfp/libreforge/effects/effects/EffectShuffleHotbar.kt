f41c87cd6d3b2d948d63fc840280557a35b0ce6b
Auxilor <william.favierparsons1@gmail.com>
2022-08-28 15:38:20 +0100

    Added shuffle_hotbar

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectShuffleHotbar.kt
@@ -0,0 +1,28 @@
+package com.willfp.libreforge.effects.effects
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import com.willfp.libreforge.triggers.Triggers
+import org.bukkit.entity.Player
+
+@Suppress("UNCHECKED_CAST")
+class EffectShuffleHotbar : Effect(
+    "shuffle_hotbar",
+    triggers = Triggers.withParameters(
+        TriggerParameter.VICTIM
+    )
+) {
+    override fun handle(data: TriggerData, config: Config) {
+        val victim = data.victim as? Player ?: return
+
+        val hotbar = (0..9)
+            .map { victim.inventory.getItem(it) }
+            .shuffled()
+
+        for ((index, item) in hotbar.withIndex()) {
+            victim.inventory.setItem(index, item)
+        }
+    }
+}
