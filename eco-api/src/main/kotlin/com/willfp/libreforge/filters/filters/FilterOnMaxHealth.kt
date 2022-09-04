50146ff5205c0844829dfe7fc48a53dde21f3ef9
Auxilor <william.favierparsons1@gmail.com>
2022-09-04 16:19:25 +0100

    Added onMaxHealth filter

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterOnMaxHealth.kt
@@ -0,0 +1,19 @@
+package com.willfp.libreforge.filters.filters
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.filters.FilterComponent
+import com.willfp.libreforge.triggers.TriggerData
+import org.bukkit.attribute.Attribute
+
+class FilterOnMaxHealth : FilterComponent() {
+    override fun passes(data: TriggerData, config: Config): Boolean {
+        val entity = data.victim ?: return true
+
+        return config.withInverse("onMaxHealth", Config::getBoolOrNull) {
+            val maxHealth = entity.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.value
+            val onMaxHealth = entity.health == maxHealth
+
+            onMaxHealth == it
+        }
+    }
+}
