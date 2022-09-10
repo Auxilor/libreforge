42dad82d2c91c91ff121dd9ccff68fcaa0e7826b
Auxilor <william.favierparsons1@gmail.com>
2022-09-10 10:38:42 +0100

    Added Counter

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/triggers/Counter.kt
@@ -0,0 +1,37 @@
+package com.willfp.libreforge.triggers
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.conditions.ConfiguredCondition
+import com.willfp.libreforge.conditions.isMet
+import com.willfp.libreforge.events.TriggerPreProcessEvent
+import com.willfp.libreforge.filters.Filter
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+
+class Counter(
+    val trigger: Trigger,
+    val multiplier: Double,
+    val conditions: Iterable<ConfiguredCondition>,
+    val filters: Config
+) {
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    fun getCount(event: TriggerPreProcessEvent): Double {
+        val player = event.player
+        val value = event.value
+        val data = event.data
+
+        if (!conditions.isMet(player)) {
+            return 0.0
+        }
+
+        if (!Filter.matches(data, filters)) {
+            return 0.0
+        }
+
+        if (event.trigger != trigger) {
+            return 0.0
+        }
+
+        return value * multiplier
+    }
+}
