9956bb29d62eeec8cbdc4b56612e5cc8adf8a5af
Auxilor <william.favierparsons1@gmail.com>
2022-09-10 10:45:21 +0100

    Added Counters

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/triggers/Counters.kt
@@ -0,0 +1,56 @@
+package com.willfp.libreforge.triggers
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.ConfigViolation
+import com.willfp.libreforge.LibReforgePlugin
+import com.willfp.libreforge.conditions.Conditions
+import com.willfp.libreforge.separatorAmbivalent
+
+@Suppress("UNUSED")
+object Counters {
+    /**
+     * Compile a counter.
+     *
+     * @param cfg The config for the effect chain.
+     * @param context The context to log violations for.
+     * @return The counter, or null if invalid.
+     */
+    @JvmStatic
+    fun compile(cfg: Config, context: String): Counter? {
+        val config = cfg.separatorAmbivalent()
+
+        val id = config.getString("trigger")
+
+        val trigger = Triggers.getById(id)
+
+        if (trigger == null) {
+            LibReforgePlugin.instance.logViolation(
+                id,
+                context,
+                ConfigViolation("trigger", "Invalid trigger ID specified!")
+            )
+
+            return null
+        }
+
+
+        val multiplier = if (config.has("multiplier")) {
+            config.getDoubleFromExpression("multiplier")
+        } else {
+            1.0
+        }
+
+        val conditions = config.getSubsections("conditions").mapNotNull {
+            Conditions.compile(it, "$context -> Conditions")
+        }
+
+        val filters = config.getSubsection("filters")
+
+        return Counter(
+            trigger,
+            multiplier,
+            conditions,
+            filters
+        )
+    }
+}
