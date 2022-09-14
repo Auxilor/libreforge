110e17eebb853595bad913fa18346d6dac901580
_OfTeN_ <dkoften@gmail.com>
2022-09-15 00:24:41 +0300

    Added scyther integration

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/scyther/TriggerScytherAutoCollect.kt
@@ -0,0 +1,32 @@
+package com.willfp.libreforge.integrations.scyther
+
+import com.willfp.eco.core.integrations.mcmmo.McmmoManager
+import com.willfp.libreforge.triggers.Trigger
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import dev.norska.scyther.api.ScytherAutocollectEvent
+import dev.norska.scyther.api.ScytherAutosellEvent
+import org.bukkit.event.EventHandler
+import org.bukkit.inventory.ItemStack
+
+class TriggerScytherAutoCollect: Trigger("scyther_auto_collect", parameters = listOf(TriggerParameter.PLAYER,
+    TriggerParameter.BLOCK, TriggerParameter.ITEM)) {
+
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: ScytherAutocollectEvent) {
+        if (McmmoManager.isFake(event)) {
+            return
+        }
+
+        if (event.isCancelled) return
+
+        this.processTrigger(
+            event.player,
+            TriggerData(
+                player = event.player,
+                block = event.block,
+                item = ItemStack(event.cropMaterial, event.dropAmount)
+            )
+        )
+    }
+}
