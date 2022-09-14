15ffaff35658a3ca41d5e560e0dfa54e77264752
_OfTeN_ <dkoften@gmail.com>
2022-09-15 00:24:41 +0300

    Added scyther integration

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/scyther/ScytherIntegration.kt
@@ -0,0 +1,19 @@
+package com.willfp.libreforge.integrations.scyther
+
+import com.willfp.eco.core.integrations.Integration
+import com.willfp.libreforge.conditions.Condition
+import com.willfp.libreforge.triggers.Trigger
+
+object ScytherIntegration: Integration {
+    private lateinit var SCYTHER_AUTO_COLLECT: Trigger
+    private lateinit var SCYTHER_AUTO_SELL: Trigger
+
+    fun load() {
+        SCYTHER_AUTO_COLLECT = TriggerScytherAutoCollect()
+        SCYTHER_AUTO_SELL = TriggerScytherAutoSell()
+    }
+
+    override fun getPluginName(): String {
+        return "Scyther"
+    }
+}
