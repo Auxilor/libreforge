aa0b78833486939cbf26610449d29a665cc1701a
Auxilor <william.favierparsons1@gmail.com>
2022-09-22 17:26:32 +0100

    Added text_contains filter

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterTextContains.kt
@@ -0,0 +1,15 @@
+package com.willfp.libreforge.filters.filters
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.filters.FilterComponent
+import com.willfp.libreforge.triggers.TriggerData
+
+class FilterTextContains : FilterComponent() {
+    override fun passes(data: TriggerData, config: Config): Boolean {
+        val text = data.text ?: return true
+
+        return config.withInverse("text_contains", Config::getStringsOrNull) {
+            it?.any { test -> text.lowercase().contains(test.lowercase()) } == true
+        }
+    }
+}
