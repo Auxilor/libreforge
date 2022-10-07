04741fdba1c0c13e4d0e256e1a3012689412af7a
Auxilor <william.favierparsons1@gmail.com>
2022-10-07 17:24:37 +0100

    Fixed aurelium skills integration

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/aureliumskills/EffectSkillXpMultiplier.kt
@@ -0,0 +1,108 @@
+package com.willfp.libreforge.integrations.aureliumskills
+
+import com.archyx.aureliumskills.api.event.XpGainEvent
+import com.archyx.aureliumskills.skills.Skill
+import com.archyx.aureliumskills.skills.Skills
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.core.integrations.mcmmo.McmmoManager
+import com.willfp.libreforge.ConfigViolation
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.effects.Identifiers
+import com.willfp.libreforge.effects.MultiplierModifier
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import java.util.UUID
+
+class EffectSkillXpMultiplier : Effect("skill_xp_multiplier") {
+    private val modifiers = mutableMapOf<UUID, MutableMap<Skill, MutableList<MultiplierModifier>>>()
+    private val globalModifiers = mutableMapOf<UUID, MutableList<MultiplierModifier>>()
+
+    override fun handleEnable(
+        player: Player,
+        config: Config,
+        identifiers: Identifiers
+    ) {
+        if (config.has("skills")) {
+            val skills = config.getStrings("skills").mapNotNull { Skills.valueOf(it.uppercase()) }
+            for (skill in skills) {
+                val skillModifiers = modifiers[player.uniqueId] ?: mutableMapOf()
+                val registeredModifiers = skillModifiers[skill] ?: mutableListOf()
+                val uuid = identifiers.uuid
+                registeredModifiers.removeIf { it.uuid == uuid }
+                registeredModifiers.add(
+                    MultiplierModifier(
+                        uuid,
+                        config.getDoubleFromExpression("multiplier", player)
+                    )
+                )
+                skillModifiers[skill] = registeredModifiers
+                modifiers[player.uniqueId] = skillModifiers
+            }
+        } else {
+            val registeredModifiers = globalModifiers[player.uniqueId] ?: mutableListOf()
+            val uuid = identifiers.uuid
+            registeredModifiers.removeIf { it.uuid == uuid }
+            registeredModifiers.add(
+                MultiplierModifier(
+                    uuid,
+                    config.getDoubleFromExpression("multiplier", player)
+                )
+            )
+            globalModifiers[player.uniqueId] = registeredModifiers
+        }
+    }
+
+    override fun handleDisable(
+        player: Player,
+        identifiers: Identifiers
+    ) {
+        val registeredModifiers = globalModifiers[player.uniqueId] ?: mutableListOf()
+        val uuid = identifiers.uuid
+        registeredModifiers.removeIf { it.uuid == uuid }
+        globalModifiers[player.uniqueId] = registeredModifiers
+
+
+        for (skill in Skills.values()) {
+            val skillModifierMap = modifiers[player.uniqueId] ?: mutableMapOf()
+            val registeredSkillModifiers = skillModifierMap[skill] ?: mutableListOf()
+            registeredSkillModifiers.removeIf { it.uuid == uuid }
+            skillModifierMap[skill] = registeredSkillModifiers
+            modifiers[player.uniqueId] = skillModifierMap
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: XpGainEvent) {
+        if (McmmoManager.isFake(event)) {
+            return
+        }
+
+        val player = event.player
+
+        var multiplier = 1.0
+
+        for (modifier in (globalModifiers[player.uniqueId] ?: emptyList())) {
+            multiplier *= modifier.multiplier
+        }
+
+
+        for (modifier in (modifiers[player.uniqueId]?.get(event.skill) ?: emptyList())) {
+            multiplier *= modifier.multiplier
+        }
+
+        event.amount *= multiplier
+    }
+
+    override fun validateConfig(config: Config): List<ConfigViolation> {
+        val violations = mutableListOf<ConfigViolation>()
+
+        if (!config.has("multiplier")) violations.add(
+            ConfigViolation(
+                "multiplier",
+                "You must specify the xp multiplier!"
+            )
+        )
+
+        return violations
+    }
+}
