a737d8b77218fb742e2dd7d4567622eeffb3e07f
Auxilor <william.favierparsons1@gmail.com>
2022-11-12 17:30:56 +0000

    Added give_price and pay_price

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectGivePrice.kt
@@ -0,0 +1,49 @@
+package com.willfp.libreforge.effects.effects
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.core.price.Prices
+import com.willfp.libreforge.ConfigViolation
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.toMathContext
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import com.willfp.libreforge.triggers.Triggers
+
+class EffectGivePrice : Effect(
+    "give_price",
+    triggers = Triggers.withParameters(
+        TriggerParameter.PLAYER
+    )
+) {
+    override fun handle(data: TriggerData, config: Config) {
+        val player = data.player ?: return
+
+        val price = Prices.create(
+            config.getString("value"),
+            config.getString("type"),
+            config.toMathContext(data)
+        )
+
+        price.giveTo(player)
+    }
+
+    override fun validateConfig(config: Config): List<ConfigViolation> {
+        val violations = mutableListOf<ConfigViolation>()
+
+        if (!config.has("value")) violations.add(
+            ConfigViolation(
+                "value",
+                "You must specify the value of the price to give!"
+            )
+        )
+
+        if (!config.has("type")) violations.add(
+            ConfigViolation(
+                "type",
+                "You must specify the type of price (coins, xpl, etc)!"
+            )
+        )
+
+        return violations
+    }
+}
