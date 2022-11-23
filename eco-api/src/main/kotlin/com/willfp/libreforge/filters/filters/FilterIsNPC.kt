64763f4b1bc1668bf0074cf558eb2894271cd66d
Auxilor <william.favierparsons1@gmail.com>
2022-11-23 13:44:27 +0000

    Cleanup

--- eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterIsNPC.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/filters/filters/FilterIsNPC.kt
@@ -1,18 +1,15 @@
 package com.willfp.libreforge.filters.filters
 
 import com.willfp.eco.core.config.interfaces.Config
-import com.willfp.libreforge.effects.effects.particles.toVector2f
 import com.willfp.libreforge.filters.Filter
 import com.willfp.libreforge.triggers.TriggerData
-import kotlin.math.roundToInt
 
 object FilterIsNPC : Filter() {
     override fun passes(data: TriggerData, config: Config): Boolean {
         val victim = data.victim ?: return true
 
         return config.withInverse("is_npc", Config::getBool) {
-            val isNPC = victim.hasMetadata("NPC")
-            isNPC == it
+            victim.hasMetadata("NPC") == it
         }
     }
 }
