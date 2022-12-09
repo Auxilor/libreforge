5985239aaf00f4de195069a09d514badcd2aec44
Auxilor <william.favierparsons1@gmail.com>
2022-12-09 16:30:43 +0000

    Added enter_bed and leave_bed triggers

---
+++ eco-api/src/main/kotlin/com/willfp/libreforge/triggers/triggers/TriggerLeaveBed.kt
@@ -0,0 +1,32 @@
+package com.willfp.libreforge.triggers.triggers
+
+import com.willfp.eco.core.integrations.mcmmo.McmmoManager
+import com.willfp.libreforge.triggers.Trigger
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerBedLeaveEvent
+
+class TriggerLeaveBed : Trigger(
+    "leave_bed", listOf(
+        TriggerParameter.PLAYER,
+        TriggerParameter.LOCATION
+    )
+) {
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: PlayerBedLeaveEvent) {
+        if (McmmoManager.isFake(event)) {
+            return
+        }
+
+        val player = event.player
+
+        this.processTrigger(
+            player,
+            TriggerData(
+                player = player,
+                location = player.location
+            )
+        )
+    }
+}
