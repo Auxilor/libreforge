a6440739d18eeb19573724236d7a750104ed9372
Auxilor <william.favierparsons1@gmail.com>
2022-12-26 13:58:26 +0100

    Fixed leave_job and has_active_Job

--- eco-api/src/main/kotlin/com/willfp/libreforge/integrations/ecojobs/ConditionHasActiveJob.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/integrations/ecojobs/ConditionHasActiveJob.kt
@@ -2,17 +2,14 @@ package com.willfp.libreforge.integrations.ecojobs
 
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.ecojobs.api.EcoJobsAPI
-import com.willfp.ecojobs.jobs.Jobs
 import com.willfp.libreforge.ConfigViolation
 import com.willfp.libreforge.conditions.Condition
 import org.bukkit.entity.Player
 
 class ConditionHasActiveJob : Condition("has_active_job") {
     override fun isConditionMet(player: Player, config: Config): Boolean {
-        return EcoJobsAPI.instance.hasJob(
-            player,
-            Jobs.getByID(config.getString("job").lowercase()) ?: return false
-        )
+        return EcoJobsAPI.instance.getActiveJob(player)?.id ==
+                config.getString("job").lowercase()
     }
 
     override fun validateConfig(config: Config): List<ConfigViolation> {
@@ -20,8 +17,7 @@ class ConditionHasActiveJob : Condition("has_active_job") {
 
         if (!config.has("job")) violations.add(
             ConfigViolation(
-                "job",
-                "You must specify the job!"
+                "job", "You must specify the job!"
             )
         )
 
