134cf70b0076c1361023db34dc663f79f7c4cd83
Auxilor <william.favierparsons1@gmail.com>
2023-01-24 09:57:01 +0000

    Fixed flight, other cleanup

--- eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectFlight.kt
+++ eco-api/src/main/kotlin/com/willfp/libreforge/effects/effects/EffectFlight.kt
@@ -4,13 +4,20 @@ import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.libreforge.effects.Effect
 import com.willfp.libreforge.effects.Identifiers
 import org.bukkit.entity.Player
+import java.util.UUID
 
-class EffectFlight: Effect("flight", supportsFilters = true) {
+class EffectFlight : Effect("flight") {
+    private val players = mutableMapOf<UUID, List<UUID>>()
+
     override fun handleEnable(player: Player, config: Config, identifiers: Identifiers) {
-        player.isFlying = true
+        players[player.uniqueId] = players.getOrDefault(player.uniqueId, emptyList()) + identifiers.uuid
+
+        player.isFlying = players[player.uniqueId]?.isNotEmpty() == true
     }
 
     override fun handleDisable(player: Player, identifiers: Identifiers) {
-        player.isFlying = false
+        players[player.uniqueId] = players.getOrDefault(player.uniqueId, emptyList()) - identifiers.uuid
+
+        player.isFlying = players[player.uniqueId].isNullOrEmpty()
     }
 }
