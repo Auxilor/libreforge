9b5adb78eadc94684bb8ce9787a2331eabbcdded
Auxilor <william.favierparsons1@gmail.com>
2023-01-26 19:07:44 +0000

    Reimplemented not met effects / lines

--- core/src/main/kotlin/com/willfp/libreforge/conditions/ConditionList.kt
+++ core/src/main/kotlin/com/willfp/libreforge/conditions/ConditionList.kt
@@ -1,6 +1,8 @@
 package com.willfp.libreforge.conditions
 
+import com.willfp.eco.util.formatEco
 import com.willfp.libreforge.DelegatedList
+import com.willfp.libreforge.triggers.DispatchedTrigger
 import org.bukkit.entity.Player
 
 /**
@@ -15,4 +17,11 @@ class ConditionList(
 
     fun areMet(player: Player): Boolean =
         this.all { it.isMet(player) }
+
+    fun triggerNotMetEffects(trigger: DispatchedTrigger) =
+        this.forEach { it.notMetEffects.trigger(trigger) }
+
+    fun getNotMetLines(player: Player): List<String> =
+        this.flatMap { it.notMetLines }
+            .map { it.formatEco(player, formatPlaceholders = true) }
 }
