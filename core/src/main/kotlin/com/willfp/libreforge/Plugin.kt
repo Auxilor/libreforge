d82bedf81d046850cb3856bc2f93e280f829bd5a
Auxilor <william.favierparsons1@gmail.com>
2023-01-28 20:45:09 +0000

    Added fallback plugins, updated loader

--- core/src/main/kotlin/com/willfp/libreforge/Plugin.kt
+++ core/src/main/kotlin/com/willfp/libreforge/Plugin.kt
@@ -3,14 +3,35 @@ package com.willfp.libreforge
 import com.willfp.eco.core.EcoPlugin
 
 internal val plugin: EcoPlugin
-    get() = LibreforgeInitializer.plugin
+    get() = LibreforgeInitializer.plugin ?: throw NoLibreforgePluginPresentError(
+        "No libreforge plugin found on the server! Debug: ${LibreforgeInitializer.plugins}"
+    )
+
+private class NoLibreforgePluginPresentError(
+    override val message: String
+) : Error(message)
 
 object LibreforgeInitializer {
-    internal lateinit var plugin: EcoPlugin
+    internal var plugin: EcoPlugin? = null
+
+    internal val plugins = mutableListOf<EcoPlugin>()
 
     fun addPlugin(plugin: EcoPlugin) {
-        if (!this::plugin.isInitialized) {
+        if (this.plugin == null) {
             this.plugin = plugin
+        }
+
+        plugins += plugin
+
+        // To prevent problems, when a libreforge plugin is disabled,
+        // the core plugin is automatically switched to the next available
+        // one.
+        plugin.onDisable {
+            plugins -= plugin
+
+            if (this.plugin == plugin) {
+                this.plugin = plugins.firstOrNull()
+            }
         }
     }
 }
