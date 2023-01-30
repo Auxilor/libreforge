47d8e8ef4142e522fae58e997a93a1b5a28bc9e5
Auxilor <william.favierparsons1@gmail.com>
2023-01-30 14:20:50 +0000

    Implemented effect arguments

---
+++ core/src/main/kotlin/com/willfp/libreforge/effects/arguments/impl/ArgumentCooldown.kt
@@ -0,0 +1,75 @@
+package com.willfp.libreforge.effects.arguments.impl
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.util.PlayerUtils
+import com.willfp.eco.util.StringUtils
+import com.willfp.eco.util.formatEco
+import com.willfp.libreforge.LibReforgePlugin
+import com.willfp.libreforge.NoCompileData
+import com.willfp.libreforge.effects.ConfiguredEffect
+import com.willfp.libreforge.effects.EffectArgument
+import com.willfp.libreforge.effects.ElementLike
+import com.willfp.libreforge.effects.arguments.EffectArgument
+import com.willfp.libreforge.getDoubleFromExpression
+import com.willfp.libreforge.triggers.DispatchedTrigger
+import com.willfp.libreforge.triggers.InvocationData
+import org.bukkit.Sound
+import java.util.UUID
+import kotlin.math.ceil
+
+object ArgumentCooldown : EffectArgument<NoCompileData>("cooldown") {
+    // Maps ConfiguredEffect UUIDs to Player UUIDs mapped to expiry time
+    private val cooldownTracker = mutableMapOf<UUID, MutableMap<UUID, Long>>()
+
+    override fun isMet(element: ElementLike, trigger: DispatchedTrigger, compileData: NoCompileData): Boolean {
+        return getCooldown(element, trigger) <= 0
+    }
+
+    private fun getCooldown(element: ElementLike, trigger: DispatchedTrigger): Int {
+        val effectEndTimes = cooldownTracker[element.uuid] ?: return 0
+        val endTime = effectEndTimes[trigger.player.uniqueId] ?: return 0
+
+        val msLeft = endTime - System.currentTimeMillis()
+        val secondsLeft = ceil(msLeft.toDouble() / 1000).toLong()
+        return secondsLeft.toInt()
+    }
+
+    override fun ifMet(element: ElementLike, trigger: DispatchedTrigger, compileData: NoCompileData) {
+        val effectEndTimes = cooldownTracker[element.uuid] ?: mutableMapOf()
+        effectEndTimes[trigger.player.uniqueId] = System.currentTimeMillis() +
+                (element.config.getDoubleFromExpression("cooldown", trigger.data) * 1000L).toLong()
+        cooldownTracker[element.uuid] = effectEndTimes
+    }
+
+    override fun ifNotMet(element: ElementLike, trigger: DispatchedTrigger, compileData: NoCompileData) {
+        if (element.config.getBoolOrNull("send_cooldown_message") == false) {
+            return
+        }
+
+        val player = trigger.player
+
+        val cooldown = getCooldown(element, trigger)
+
+        val message = element.config.getStringOrNull("cooldown_message")
+            ?.replace("%seconds%", cooldown.toString())
+            ?.formatEco(trigger.player, true)
+
+            ?: plugin.langYml.getMessage("on-cooldown")
+                .replace("%seconds%", cooldown.toString())
+
+        if (plugin.configYml.getBool("cooldown.in-actionbar")) {
+            PlayerUtils.getAudience(player).sendActionBar(StringUtils.toComponent(message))
+        } else {
+            player.sendMessage(message)
+        }
+
+        if (plugin.configYml.getBool("cooldown.sound.enabled")) {
+            player.playSound(
+                player.location,
+                Sound.valueOf(plugin.configYml.getString("cooldown.sound.sound").uppercase()),
+                1.0f,
+                plugin.configYml.getDouble("cooldown.sound.pitch").toFloat()
+            )
+        }
+    }
+}
---
+++ core/src/main/kotlin/com/willfp/libreforge/effects/arguments/impl/ArgumentPrice.kt
@@ -0,0 +1,75 @@
+package com.willfp.libreforge.effects.arguments.impl
+
+import com.willfp.eco.core.price.Prices
+import com.willfp.eco.util.NumberUtils
+import com.willfp.eco.util.PlayerUtils
+import com.willfp.eco.util.StringUtils
+import com.willfp.libreforge.NoCompileData
+import com.willfp.libreforge.effects.ElementLike
+import com.willfp.libreforge.effects.arguments.EffectArgument
+import com.willfp.libreforge.plugin
+import com.willfp.libreforge.toMathContext
+import com.willfp.libreforge.triggers.DispatchedTrigger
+import org.bukkit.Sound
+
+object ArgumentPrice : EffectArgument<NoCompileData>("price") {
+
+    /*
+
+    I'll need to update prices to support copying with new base contexts.
+
+    In the meantime, it will have to compile each time. Very annoying.
+
+     */
+
+    override fun isMet(element: ElementLike, trigger: DispatchedTrigger, compileData: NoCompileData): Boolean {
+        val price = Prices.create(
+            element.config.getString("price.value"),
+            element.config.getString("price.type"),
+            element.config.toMathContext(trigger.data)
+        )
+
+        return price.canAfford(trigger.player)
+    }
+
+    override fun ifMet(element: ElementLike, trigger: DispatchedTrigger, compileData: NoCompileData) {
+        val price = Prices.create(
+            element.config.getString("price.value"),
+            element.config.getString("price.type"),
+            element.config.toMathContext(trigger.data)
+        )
+
+        price.pay(trigger.player)
+    }
+
+    override fun ifNotMet(element: ElementLike, trigger: DispatchedTrigger, compileData: NoCompileData) {
+        val price = Prices.create(
+            element.config.getString("price.value"),
+            element.config.getString("price.type"),
+            element.config.toMathContext(trigger.data)
+        )
+
+        price.pay(trigger.player)
+
+        val display = element.config.getString("price.display")
+            .replace("%value%", NumberUtils.format(price.getValue(trigger.player)))
+
+        val message = plugin.langYml.getMessage("cannot-afford-price")
+            .replace("%price%", display)
+
+        if (plugin.configYml.getBool("cannot-afford-price.in-actionbar")) {
+            PlayerUtils.getAudience(trigger.player).sendActionBar(StringUtils.toComponent(message))
+        } else {
+            trigger.player.sendMessage(message)
+        }
+
+        if (plugin.configYml.getBool("cannot-afford-price.sound.enabled")) {
+            trigger.player.playSound(
+                trigger.player.location,
+                Sound.valueOf(plugin.configYml.getString("cannot-afford-price.sound.sound").uppercase()),
+                1.0f,
+                plugin.configYml.getDouble("cannot-afford-price.sound.pitch").toFloat()
+            )
+        }
+    }
+}
