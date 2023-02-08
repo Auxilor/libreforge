83e7c07ff01be95abd1d9c49d3dc6f21e877fc83
Auxilor <william.favierparsons1@gmail.com>
2023-02-08 18:32:33 +0000

    Removed v3 module

--- eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionHasItem.kt
+++
@@ -1,7 +0,0 @@
-package com.willfp.libreforge.conditions.conditions
-
-import com.willfp.libreforge.conditions.GenericItemCondition
-
-class ConditionHasItem : GenericItemCondition("has_item", {
-    it.inventory.contents.toList()
-})
--- eco-api/src/main/kotlin/com/willfp/libreforge/conditions/conditions/ConditionInMainhand.kt
+++
@@ -1,7 +0,0 @@
-package com.willfp.libreforge.conditions.conditions
-
-import com.willfp.libreforge.conditions.GenericItemCondition
-
-class ConditionInMainhand : GenericItemCondition("in_mainhand", {
-    listOf(it.inventory.itemInMainHand)
-})
