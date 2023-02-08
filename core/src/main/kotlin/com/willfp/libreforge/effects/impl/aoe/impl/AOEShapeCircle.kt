68965962c6dc849ab6cf2fc521f5f5e315c853c3
Auxilor <william.favierparsons1@gmail.com>
2023-02-08 14:49:11 +0000

    Reimplemented AOE, moved to kotlin-math

---
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/aoe/impl/AOEShapeCircle.kt
@@ -0,0 +1,32 @@
+package com.willfp.libreforge.effects.impl.aoe.impl
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.NoCompileData
+import com.willfp.libreforge.arguments
+import com.willfp.libreforge.effects.impl.aoe.AOEShape
+import com.willfp.libreforge.getDoubleFromExpression
+import com.willfp.libreforge.toLocation
+import com.willfp.libreforge.triggers.TriggerData
+import dev.romainguy.kotlin.math.Float3
+import org.bukkit.World
+import org.bukkit.entity.LivingEntity
+
+object AOEShapeCircle: AOEShape<NoCompileData>("circle") {
+    override val arguments = arguments {
+        require("radius", "You must specify the circle radius!")
+    }
+
+    override fun getEntities(
+        location: Float3,
+        direction: Float3,
+        world: World,
+        config: Config,
+        data: TriggerData,
+        compileData: NoCompileData
+    ): Collection<LivingEntity> {
+        val radius = config.getDoubleFromExpression("radius", data)
+
+        return location.toLocation(world).getNearbyEntities(radius, radius, radius)
+            .filterIsInstance<LivingEntity>()
+    }
+}
---
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/aoe/impl/AOEShapeScanInFront.kt
@@ -0,0 +1,50 @@
+package com.willfp.libreforge.effects.impl.aoe.impl
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.NoCompileData
+import com.willfp.libreforge.arguments
+import com.willfp.libreforge.effects.impl.aoe.AOEShape
+import com.willfp.libreforge.getDoubleFromExpression
+import com.willfp.libreforge.getIntFromExpression
+import com.willfp.libreforge.normalize
+import com.willfp.libreforge.plusAssign
+import com.willfp.libreforge.toLocation
+import com.willfp.libreforge.triggers.TriggerData
+import dev.romainguy.kotlin.math.Float3
+import org.bukkit.World
+import org.bukkit.entity.LivingEntity
+
+object AOEShapeScanInFront: AOEShape<NoCompileData>("scan_in_front") {
+    override val arguments = arguments {
+        require("radius", "You must specify the scan radius!")
+        require("max_distance", "You must specify the maximum distance!")
+    }
+
+    override fun getEntities(
+        location: Float3,
+        direction: Float3,
+        world: World,
+        config: Config,
+        data: TriggerData,
+        compileData: NoCompileData
+    ): Collection<LivingEntity> {
+        val maxDistance = config.getIntFromExpression("max_distance", data)
+        val radius = config.getDoubleFromExpression("radius", data)
+
+        val offset = direction.normalize()
+
+        for (i in 0..maxDistance) {
+            val entities = location.toLocation(world).getNearbyEntities(radius, radius, radius)
+                .filterIsInstance<LivingEntity>()
+                .filterNot { it.uniqueId == data.player?.uniqueId }
+
+            if (entities.isNotEmpty()) {
+                return entities
+            }
+
+            location += offset
+        }
+
+        return emptyList()
+    }
+}
