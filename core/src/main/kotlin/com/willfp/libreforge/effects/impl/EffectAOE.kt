48e7884f074f8792a9b2d214b1501e7bdc4b9df2
Auxilor <william.favierparsons1@gmail.com>
2023-02-08 14:49:11 +0000

    Reimplemented AOE, moved to kotlin-math

--- core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectAOE.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectAOE.kt
@@ -6,14 +6,14 @@ import com.willfp.libreforge.arguments
 import com.willfp.libreforge.effects.Chain
 import com.willfp.libreforge.effects.Effect
 import com.willfp.libreforge.effects.Effects
-import com.willfp.libreforge.effects.effects.aoe.AOEShapes
-import com.willfp.libreforge.effects.effects.particles.toVector3f
-import com.willfp.libreforge.effects.invoke
+import com.willfp.libreforge.effects.impl.aoe.AOEBlock
+import com.willfp.libreforge.effects.impl.aoe.AOEShapes
 import com.willfp.libreforge.effects.triggerers.impl.NormalTriggererFactory
+import com.willfp.libreforge.toFloat3
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
 
-object EffectAOE : Effect<Chain?>("aoe") {
+object EffectAOE : Effect<EffectAOE.AOEData>("aoe") {
     override val parameters = setOf(
         TriggerParameter.PLAYER
     )
@@ -26,26 +26,35 @@ object EffectAOE : Effect<Chain?>("aoe") {
         inherit { AOEShapes.getByID(it.getString("shape")) }
     }
 
-    override fun onTrigger(config: Config, data: TriggerData, compileData: Chain?): Boolean {
+    override fun onTrigger(config: Config, data: TriggerData, compileData: AOEData): Boolean {
         val player = data.player ?: return false
-        val shape = AOEShapes.getByKD(config.getString("shape")) ?: return false
+        val shape = compileData.shape ?: return false
 
         for (entity in shape.getEntities(
-            player.location.toVector3f(),
-            player.eyeLocation.direction.toVector3f(),
+            player.location.toFloat3(),
+            player.eyeLocation.direction.toFloat3(),
             player.location.world,
-            config,
-            invocation.data
+            data
         ).filter { it != player }) {
-            compileData?.trigger(data.dispatch(player))
+            compileData.chain?.trigger(data.dispatch(player))
         }
+
+        return true
     }
 
-    override fun makeCompileData(config: Config, context: ViolationContext): Chain? {
-        return Effects.compileChain(
-            config.getSubsections("effects"),
-            NormalTriggererFactory.create(),
-            context.with("aoe Effects")
+    override fun makeCompileData(config: Config, context: ViolationContext): AOEData {
+        return AOEData(
+            AOEShapes.compile(config, context),
+            Effects.compileChain(
+                config.getSubsections("effects"),
+                NormalTriggererFactory.create(),
+                context.with("aoe Effects")
+            )
         )
     }
+
+    data class AOEData(
+        val shape: AOEBlock<*>?,
+        val chain: Chain?
+    )
 }
