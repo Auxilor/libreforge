7d1fd5d12bfa093202da8c1abdd5784c0fe794b1
Auxilor <william.favierparsons1@gmail.com>
2023-02-09 16:35:35 +0000

    Registered effects

--- core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectArmor.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectArmor.kt
@@ -2,12 +2,12 @@ package com.willfp.libreforge.effects.impl
 
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.libreforge.arguments
-import com.willfp.libreforge.effects.GenericAttributeEffect
+import com.willfp.libreforge.effects.templates.AttributeEffect
 import org.bukkit.attribute.Attribute
 import org.bukkit.attribute.AttributeModifier
 import org.bukkit.entity.Player
 
-object EffectArmor : GenericAttributeEffect(
+object EffectArmor : AttributeEffect(
     "armor",
     Attribute.GENERIC_ARMOR,
     AttributeModifier.Operation.ADD_NUMBER
