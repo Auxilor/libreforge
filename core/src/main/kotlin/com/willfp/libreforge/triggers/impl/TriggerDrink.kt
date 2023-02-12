d89e6597a1cdbf59d138a793801f21779d2636f2
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2023-02-12 01:56:17 +0000

    Added new Trigger for Drinking and Changing Chunk.

---
+++ core/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerDrink.kt
@@ -0,0 +1,42 @@
+package com.willfp.libreforge.triggers.impl
+
+import com.willfp.libreforge.triggers.Trigger
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerItemConsumeEvent
+
+private fun Material.isDrink(): Boolean {
+    return  this == Material.BEETROOT_SOUP ||
+            this == Material.MUSHROOM_STEW ||
+            this == Material.SUSPICIOUS_STEW ||
+            this == Material.RABBIT_STEW ||
+            this == Material.POTION ||
+            this == Material.HONEY_BOTTLE ||
+            this == Material.GLASS_BOTTLE
+}
+
+object TriggerDrink : Trigger("drink") {
+    override val parameters = setOf(
+        TriggerParameter.PLAYER,
+        TriggerParameter.EVENT,
+        TriggerParameter.ITEM
+    )
+
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: PlayerItemConsumeEvent) {
+        if (!event.item.type.isDrink()) {
+            return
+        }
+        val player = event.player
+        this.dispatch(
+            player,
+            TriggerData(
+                player = player,
+                event = event,
+                item = event.item
+            )
+        )
+    }
+}
