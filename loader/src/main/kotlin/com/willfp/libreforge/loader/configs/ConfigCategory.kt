410ea6c8d85f113170691afc5ce2d43ad271027b
Auxilor <william.favierparsons1@gmail.com>
2023-03-13 15:51:58 +0000

    Fixed ConfigCategory

--- loader/src/main/kotlin/com/willfp/libreforge/loader/configs/ConfigCategory.kt
+++ loader/src/main/kotlin/com/willfp/libreforge/loader/configs/ConfigCategory.kt
@@ -1,13 +1,19 @@
 package com.willfp.libreforge.loader.configs
 
+import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.libreforge.configs.LibreforgeConfigCategory
 
 abstract class ConfigCategory(
-    id: String,
+    private val id: String,
     val directory: String
 ) {
-    internal val handle = LibreforgeConfigCategory(id, directory)
+    internal lateinit var handle: LibreforgeConfigCategory
+        private set
+
+    internal fun makeHandle(plugin: EcoPlugin) {
+        handle = LibreforgeConfigCategory(id, directory, plugin)
+    }
 
     /**
      * Accept a config found in the directory (e.g. load it).
