30048e2d19635ae7224ee04460b9df2d29aad7d0
Auxilor <william.favierparsons1@gmail.com>
2023-03-28 22:14:04 +0100

    Improved NotNullMap

--- core/src/main/kotlin/com/willfp/libreforge/NotNullMap.kt
+++ core/src/main/kotlin/com/willfp/libreforge/NotNullMap.kt
@@ -3,17 +3,32 @@ package com.willfp.libreforge
 /**
  * Map that throws NPE if key is not present.
  */
-class NotNullMap<K, V>(private val handle: MutableMap<K, V>) : MutableMap<K, V> by handle {
+class NotNullMap<K, V>(private val handle: Map<K, V>) : Map<K, V> by handle {
     override fun get(key: K): V {
         return handle[key]!!
     }
 }
 
 inline fun <reified K, reified V> Map<K, V>.toNotNullMap(): NotNullMap<K, V> =
-    NotNullMap(this.toMutableMap())
-
-inline fun <reified K, reified V> notNullMapOf(): NotNullMap<K, V> =
-    mutableMapOf<K, V>().toNotNullMap()
+    NotNullMap(this)
 
 inline fun <reified K, reified V> notNullMapOf(vararg pairs: Pair<K, V>): NotNullMap<K, V> =
+    mapOf(*pairs).toNotNullMap()
+
+/**
+ * Mutable version of [NotNullMap].
+ */
+class NotNullMutableMap<K, V>(private val handle: MutableMap<K, V>) : MutableMap<K, V> by handle {
+    override fun get(key: K): V {
+        return handle[key]!!
+    }
+}
+
+inline fun <reified K, reified V> Map<K, V>.toNotNullMutableMap(): NotNullMap<K, V> =
+    NotNullMap(this)
+
+inline fun <reified K, reified V> notNullMutableMapOf(): NotNullMap<K, V> =
+    mutableMapOf<K, V>().toNotNullMap()
+
+inline fun <reified K, reified V> notNullMutableMapOf(vararg pairs: Pair<K, V>): NotNullMap<K, V> =
     mutableMapOf(*pairs).toNotNullMap()
