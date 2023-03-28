f65d49dd9592e41ca314bb8c36e3942788058526
Auxilor <william.favierparsons1@gmail.com>
2023-03-28 16:56:09 +0100

    Fixed entities filter

--- core/src/main/kotlin/com/willfp/libreforge/filters/impl/FilterEntities.kt
+++ core/src/main/kotlin/com/willfp/libreforge/filters/impl/FilterEntities.kt
@@ -1,8 +1,10 @@
 package com.willfp.libreforge.filters.impl
 
 import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.core.entities.Entities
 import com.willfp.eco.core.entities.TestableEntity
 import com.willfp.eco.util.containsIgnoreCase
+import com.willfp.libreforge.ViolationContext
 import com.willfp.libreforge.filters.Filter
 import com.willfp.libreforge.triggers.TriggerData
 
@@ -17,5 +19,13 @@ object FilterEntities : Filter<Collection<TestableEntity>, List<String>>("entiti
         return value.containsIgnoreCase(victim.type.name)
                 || value.containsIgnoreCase(victim.category.name)
                 || compileData.any { it.matches(victim) }
+    }
+
+    override fun makeCompileData(
+        config: Config,
+        context: ViolationContext,
+        values: List<String>
+    ): Collection<TestableEntity> {
+        return values.map { Entities.lookup(it) }
     }
 }
