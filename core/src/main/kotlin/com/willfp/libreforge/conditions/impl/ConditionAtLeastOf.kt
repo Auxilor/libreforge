af0a48f751b7143fe3ff25261cf5825437a9001d
Auxilor <william.favierparsons1@gmail.com>
2023-03-29 19:00:20 +0100

    Fixed any_of and at_least_of

--- core/src/main/kotlin/com/willfp/libreforge/conditions/impl/ConditionAtLeastOf.kt
+++ core/src/main/kotlin/com/willfp/libreforge/conditions/impl/ConditionAtLeastOf.kt
@@ -1,6 +1,7 @@
 package com.willfp.libreforge.conditions.impl
 
 import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.ProvidedHolder
 import com.willfp.libreforge.ViolationContext
 import com.willfp.libreforge.arguments
 import com.willfp.libreforge.conditions.Condition
@@ -14,8 +15,8 @@ object ConditionAtLeastOf : Condition<ConditionList>("at_least_of") {
         require("amount", "You must specify the minimum amount of conditions to meet!")
     }
 
-    override fun isMet(player: Player, config: Config, compileData: ConditionList): Boolean {
-        return compileData.count { it.isMet(player) } >= config.getIntFromExpression("amount", player)
+    override fun isMet(player: Player, config: Config, holder: ProvidedHolder, compileData: ConditionList): Boolean {
+        return compileData.count { it.isMet(player, holder) } >= config.getIntFromExpression("amount", player)
     }
 
     override fun makeCompileData(config: Config, context: ViolationContext): ConditionList {
