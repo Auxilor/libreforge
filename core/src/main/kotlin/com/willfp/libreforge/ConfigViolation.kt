054d82542d680068f159e38cadfbd54237f82169
Auxilor <william.favierparsons1@gmail.com>
2023-04-20 18:20:19 +0100

    Added ViolationLogger and SilentViolationContext

--- core/src/main/kotlin/com/willfp/libreforge/ConfigViolation.kt
+++ core/src/main/kotlin/com/willfp/libreforge/ConfigViolation.kt
@@ -8,37 +8,27 @@ import com.willfp.eco.core.EcoPlugin
 data class ConfigViolation(val param: String, val message: String)
 
 /**
- * A context in which a violation occurred.
+ * A logger for violations.
  */
-class ViolationContext {
-    private val plugin: EcoPlugin
-
-    private val parents: List<String>
-
-    constructor(plugin: EcoPlugin) {
-        this.plugin = plugin
-        parents = listOf()
-    }
-
-    constructor(plugin: EcoPlugin, context: String) {
-        this.plugin = plugin
-        parents = listOf(context)
-    }
-
-    internal constructor(plugin: EcoPlugin, parents: List<String>) {
-        this.plugin = plugin
-        this.parents = parents
-    }
-
+interface ViolationLogger {
     /**
-     * Copy the violation context with an extra added context.
+     * Log a violation.
      */
-    fun with(context: String) = ViolationContext(plugin, parents + context)
+    fun log(property: Compilable<*>, violation: ConfigViolation)
 
     /**
      * Log a violation.
      */
-    fun log(property: Compilable<*>, violation: ConfigViolation) {
+    fun log(violation: ConfigViolation)
+}
+
+/**
+ * A violation logger that logs to the plugin logger.
+ */
+class PluginViolationLogger(
+    private val plugin: EcoPlugin
+) : ViolationLogger {
+    override fun log(property: Compilable<*>, violation: ConfigViolation) {
         plugin.logger.warning("")
         plugin.logger.warning("Invalid configuration for ${property.id} found at $this:")
         plugin.logger.warning("(Cause) Argument '${violation.param}'")
@@ -46,18 +36,70 @@ class ViolationContext {
         plugin.logger.warning("")
     }
 
-    /**
-     * Log a violation.
-     */
-    fun log(violation: ConfigViolation) {
+    override fun log(violation: ConfigViolation) {
         plugin.logger.warning("")
         plugin.logger.warning("Invalid configuration found at $this:")
         plugin.logger.warning("(Cause) Argument '${violation.param}'")
         plugin.logger.warning("(Reason) ${violation.message}")
         plugin.logger.warning("")
     }
+}
+
+
+/**
+ * A context in which a violation occurred.
+ */
+open class ViolationContext internal constructor(
+    private val plugin: EcoPlugin,
+    private val parents: List<String>,
+    protected open val logger: ViolationLogger = PluginViolationLogger(plugin)
+) {
+    constructor(plugin: EcoPlugin) : this(plugin, emptyList())
+    constructor(plugin: EcoPlugin, context: String) : this(plugin, listOf(context))
+
+    /**
+     * Copy the violation context with an extra added context.
+     */
+    fun with(context: String) = ViolationContext(plugin, parents + context, logger)
+
+    /**
+     * Log a violation.
+     */
+    fun log(property: Compilable<*>, violation: ConfigViolation) {
+        logger.log(property, violation)
+        plugin.logger.warning("")
+    }
+
+    /**
+     * Log a violation.
+     */
+    fun log(violation: ConfigViolation) {
+        logger.log(violation)
+    }
 
     override fun toString(): String {
         return parents.joinToString(" -> ")
     }
 }
+
+/**
+ * A violation logger that does not log.
+ */
+object SilentViolationLogger : ViolationLogger {
+    override fun log(property: Compilable<*>, violation: ConfigViolation) {
+        // Do nothing
+    }
+
+    override fun log(violation: ConfigViolation) {
+        // Do nothing
+    }
+}
+
+/**
+ * A violation context that does not log.
+ */
+object SilentViolationContext : ViolationContext(
+    plugin,
+    emptyList(),
+    SilentViolationLogger
+)
