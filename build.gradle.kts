3ab45e9b5bca3fcdcc975fc8f80cb442855f2a92
Auxilor <william.favierparsons1@gmail.com>
2023-04-20 19:53:58 +0100

    Fixed repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,9 +28,10 @@ allprojects {
     repositories {
         mavenCentral()
         mavenLocal()
+        maven("https://repo.auxilor.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repo/repository/maven-public/")
         maven("https://jitpack.io")
         maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-        maven("https://papermc.io/repo/repository/maven-public/")
         maven("https://repo.codemc.io/repository/maven-public/")
         maven("https://maven.enginehub.org/repo/")
         maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
