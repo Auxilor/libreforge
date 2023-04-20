47456435590e639a1d3da334c1c1f8da4e192b83
Auxilor <william.favierparsons1@gmail.com>
2023-04-20 19:24:49 +0100

    Updated to use KRegistrable

--- core/src/main/kotlin/com/willfp/libreforge/configs/LibreforgeConfigCategory.kt
+++ core/src/main/kotlin/com/willfp/libreforge/configs/LibreforgeConfigCategory.kt
@@ -1,16 +1,12 @@
 package com.willfp.libreforge.configs
 
 import com.willfp.eco.core.EcoPlugin
-import com.willfp.eco.core.registry.Registrable
+import com.willfp.eco.core.registry.KRegistrable
 import com.willfp.eco.core.registry.Registry
 
 data class LibreforgeConfigCategory(
-    val id: String,
+    override val id: String,
     val directory: String,
     val supportsSharing: Boolean,
     val plugin: EcoPlugin
-): Registry<LibreforgeObjectConfig>(), Registrable {
-    override fun getID(): String {
-        return id
-    }
-}
+): Registry<LibreforgeObjectConfig>(), KRegistrable
