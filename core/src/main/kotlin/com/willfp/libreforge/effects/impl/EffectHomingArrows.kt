fbbca7d3ca23a631545a48623f39b4685efba464
Auxilor <william.favierparsons1@gmail.com>
2023-05-03 23:01:17 +0100

    Made homing arrows a triggered effect

--- core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectHomingArrows.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectHomingArrows.kt
@@ -1,20 +1,21 @@
 package com.willfp.libreforge.effects.impl
 
 import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.core.entities.Entities
+import com.willfp.eco.core.entities.TestableEntity
 import com.willfp.eco.core.integrations.antigrief.AntigriefManager
-import com.willfp.eco.core.map.listMap
-import com.willfp.libreforge.NoCompileData
-import com.willfp.libreforge.ProvidedHolder
+import com.willfp.libreforge.ViolationContext
 import com.willfp.libreforge.arguments
 import com.willfp.libreforge.distance
-import com.willfp.libreforge.effects.AdditionModifier
 import com.willfp.libreforge.effects.Effect
-import com.willfp.libreforge.effects.Identifiers
+import com.willfp.libreforge.getDoubleFromExpression
 import com.willfp.libreforge.lerp
 import com.willfp.libreforge.normalize
 import com.willfp.libreforge.plugin
 import com.willfp.libreforge.toFloat3
 import com.willfp.libreforge.toVector
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
 import org.bukkit.GameMode
 import org.bukkit.entity.AbstractArrow
 import org.bukkit.entity.EntityType
@@ -22,60 +23,75 @@ import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import org.bukkit.entity.Trident
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.entity.ProjectileLaunchEvent
-import java.util.UUID
 import kotlin.math.abs
 
-object EffectHomingArrows : Effect<NoCompileData>("homing_arrows") {
+object EffectHomingArrows : Effect<List<TestableEntity>>("homing_arrows") {
     override val arguments = arguments {
         require("distance", "You must specify the distance to hone from!")
     }
 
-    private val modifiers = listMap<UUID, AdditionModifier>()
-    private const val maxChecks = 10
-    private const val checkDelay = 3L
-    private const val smoothness = 0.35f
+    override val parameters = setOf(
+        TriggerParameter.PROJECTILE
+    )
 
-    override fun onEnable(
-        player: Player,
-        config: Config,
-        identifiers: Identifiers,
-        holder: ProvidedHolder,
-        compileData: NoCompileData
-    ) {
-        modifiers[player.uniqueId] += AdditionModifier(identifiers.uuid) {
-            config.getDoubleFromExpression("distance", player)
-        }
-    }
+    private const val META_KEY_DISTANCE = "libreforge-homing-arrows-distance"
+    private const val META_KEY_FORCE = "libreforge-homing-arrows-force"
+    private const val META_KEY_TARGETS = "libreforge-homing-arrows-targets"
+    private const val MAX_CHECKS = 10
+    private const val CHECK_DELAY = 3L
+    private const val SMOOTHNESS = 0.35f
 
-    override fun onDisable(player: Player, identifiers: Identifiers, holder: ProvidedHolder) {
-        modifiers[player.uniqueId].removeIf { it.uuid == identifiers.uuid }
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    fun handle(event: ProjectileLaunchEvent) {
-        val arrow = event.entity as? AbstractArrow ?: return
-        val player = arrow.shooter as? Player ?: return
+    override fun onTrigger(config: Config, data: TriggerData, compileData: List<TestableEntity>): Boolean {
+        val arrow = data.projectile as? AbstractArrow ?: return false
 
         if (arrow is Trident) {
-            return
+            return false
         }
 
-        var distance = modifiers[player.uniqueId].sumOf { it.bonus }
+        var distance = config.getDoubleFromExpression("distance", data)
 
         if (distance < 0.5) {
-            return
+            return false
         }
 
         val force = (arrow.velocity.clone().length() / 3).coerceAtMost(1.0).toFloat()
         distance *= force
 
+        val targets = compileData.toMutableList()
+
+        if (arrow.hasMetadata(META_KEY_DISTANCE)) {
+            distance += arrow.getMetadata(META_KEY_DISTANCE).firstOrNull()?.value() as? Double ?: 0.0
+            @Suppress("UNCHECKED_CAST")
+            targets += arrow.getMetadata(META_KEY_DISTANCE).firstOrNull()?.value() as? List<TestableEntity>
+                ?: emptyList()
+        }
+
+        arrow.setMetadata(META_KEY_DISTANCE, plugin.createMetadataValue(distance))
+        arrow.setMetadata(META_KEY_FORCE, plugin.createMetadataValue(force))
+        arrow.setMetadata(META_KEY_TARGETS, plugin.createMetadataValue(targets))
+
+        return true
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    fun handle(event: ProjectileLaunchEvent) {
+        val arrow = event.entity as? AbstractArrow ?: return
+        val player = arrow.shooter as? Player ?: return
+
         var checks = 0
+
+        val distance = arrow.getMetadata(META_KEY_DISTANCE).firstOrNull()?.value() as? Double ?: return
+        val force = arrow.getMetadata(META_KEY_FORCE).firstOrNull()?.value() as? Float ?: return
+
+        @Suppress("UNCHECKED_CAST")
+        val targets = arrow.getMetadata(META_KEY_TARGETS).firstOrNull()?.value() as? List<TestableEntity> ?: return
 
         plugin.runnableFactory.create { task ->
             checks++
 
-            if (checks > maxChecks) {
+            if (checks > MAX_CHECKS) {
                 task.cancel()
             }
 
@@ -90,6 +106,7 @@ object EffectHomingArrows : Effect<NoCompileData>("homing_arrows") {
                 .filterNot { it.type in setOf(EntityType.ENDERMAN, EntityType.ARMOR_STAND) }
                 .filterNot { it.isDead }
                 .filter { AntigriefManager.canInjure(player, it) }
+                .filter { if (targets.isNotEmpty()) targets.any { t -> t.matches(it) } else true }
                 .filter { if (it is Player) it.gameMode in setOf(GameMode.ADVENTURE, GameMode.SURVIVAL) else true }
                 .sortedBy { it.location.distanceSquared(arrow.location) }
 
@@ -122,9 +139,15 @@ object EffectHomingArrows : Effect<NoCompileData>("homing_arrows") {
                 }
 
                 // Lerp between the current velocity and the target velocity
-                arrow.velocity = lerp(arrow.velocity.toFloat3(), targetVelocity, 1 - smoothness).toVector()
+                arrow.velocity = lerp(arrow.velocity.toFloat3(), targetVelocity, 1 - SMOOTHNESS).toVector()
             }
 
-        }.runTaskTimer(3L, checkDelay)
+        }.runTaskTimer(3L, CHECK_DELAY)
+    }
+
+    override fun makeCompileData(config: Config, context: ViolationContext): List<TestableEntity> {
+        return config.getStrings("targets").map {
+            Entities.lookup(it)
+        }
     }
 }
