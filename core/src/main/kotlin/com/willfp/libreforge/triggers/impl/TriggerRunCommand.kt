f8e002cb4109cb11b9b6d3e864b537beed999987
Auxilor <william.favierparsons1@gmail.com>
2023-05-09 17:42:00 +0100

    Added run_command trigger

---
+++ core/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerRunCommand.kt
@@ -0,0 +1,32 @@
+package com.willfp.libreforge.triggers.impl
+
+import com.willfp.libreforge.plugin
+import com.willfp.libreforge.triggers.Trigger
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerCommandPreprocessEvent
+
+object TriggerRunCommand : Trigger("run_command") {
+    override val parameters = setOf(
+        TriggerParameter.PLAYER,
+        TriggerParameter.LOCATION,
+        TriggerParameter.TEXT
+    )
+
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: PlayerCommandPreprocessEvent) {
+        val player = event.player
+
+        plugin.scheduler.run {
+            this.dispatch(
+                player,
+                TriggerData(
+                    player = player,
+                    location = player.location,
+                    text = event.message
+                )
+            )
+        }
+    }
+}
