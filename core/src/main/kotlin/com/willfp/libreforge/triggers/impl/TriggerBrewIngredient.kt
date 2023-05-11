2760c544e40b574fe011f872400f835388e523fc
Auxilor <william.favierparsons1@gmail.com>
2023-05-11 18:01:54 +0100

    Added brew_ingredient trigger (same as brew, but passes the ingredient instead of the potion)

--- core/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerBrewIngredient.kt
+++ core/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerBrewIngredient.kt
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.inventory.BrewEvent
 
-object TriggerBrew : Trigger("brew") {
+object TriggerBrewIngredient : Trigger("brew_ingredient") {
     override val parameters = setOf(
         TriggerParameter.PLAYER,
         TriggerParameter.LOCATION,
@@ -18,9 +18,6 @@ object TriggerBrew : Trigger("brew") {
     @EventHandler(ignoreCancelled = true)
     fun handle(event: BrewEvent) {
         val player = event.contents.viewers.firstOrNull() as? Player ?: return
-        val item = (0..2).map { event.contents.getItem(it) }
-            .filterNot { EmptyTestableItem().matches(it) }
-            .firstOrNull()
 
         val amount = (0..2).map { event.contents.getItem(it) }
             .count { !EmptyTestableItem().matches(it) }
@@ -29,7 +26,7 @@ object TriggerBrew : Trigger("brew") {
             player,
             TriggerData(
                 player = player,
-                item = item,
+                item = event.contents.ingredient,
                 value = amount.toDouble()
             )
         )
