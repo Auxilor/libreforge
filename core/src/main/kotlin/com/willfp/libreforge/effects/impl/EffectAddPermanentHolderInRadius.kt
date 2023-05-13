947909a1dde577bb780098c7ce42225c939ea4fc
Auxilor <william.favierparsons1@gmail.com>
2023-05-13 17:01:44 +0100

    Conditions are no longer required in add_holder effects

--- core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectAddPermanentHolderInRadius.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectAddPermanentHolderInRadius.kt
@@ -22,7 +22,7 @@ import java.util.concurrent.TimeUnit
 object EffectAddPermanentHolderInRadius : Effect<HolderTemplate>("add_permanent_holder_in_radius") {
     override val arguments = arguments {
         require("effects", "You must specify the effects!")
-        require("conditions", "You must specify the conditions!")
+        // require("conditions", "You must specify the conditions!")
         require("radius", "You must specify the radius!")
     }
 
