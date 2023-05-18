97399b477ccc6f13471480f05b7b225415a90e4a
Auxilor <william.favierparsons1@gmail.com>
2023-05-18 01:09:11 +0100

    Added add_enchant, remove_enchant, and slot parameter to give_item

---
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectAddEnchant.kt
@@ -0,0 +1,38 @@
+package com.willfp.libreforge.effects.impl
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.NoCompileData
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.getIntFromExpression
+import com.willfp.libreforge.getProvider
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.NamespacedKey
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.EnchantmentStorageMeta
+
+object EffectAddEnchant : Effect<NoCompileData>("add_enchant") {
+    override val parameters = setOf(
+        TriggerParameter.ITEM
+    )
+
+    override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
+        val item = data.holder.getProvider<ItemStack>() ?: data.item ?: return false
+
+        val meta = item.itemMeta ?: return false
+
+        val enchant = Enchantment.getByKey(NamespacedKey.minecraft(config.getString("enchant"))) ?: return false
+        val level = config.getIntFromExpression("level", data)
+
+        if (meta is EnchantmentStorageMeta) {
+            meta.addStoredEnchant(enchant, level, true)
+        } else {
+            meta.addEnchant(enchant, level, true)
+        }
+
+        item.itemMeta = meta
+
+        return true
+    }
+}
--- core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectGiveItem.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectGiveItem.kt
@@ -3,11 +3,13 @@ package com.willfp.libreforge.effects.impl
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.eco.core.drops.DropQueue
 import com.willfp.eco.core.items.Items
+import com.willfp.eco.core.recipe.parts.EmptyTestableItem
 import com.willfp.libreforge.ViolationContext
 import com.willfp.libreforge.arguments
 import com.willfp.libreforge.effects.Effect
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
 object EffectGiveItem : Effect<ItemStack>("give_item") {
@@ -22,15 +24,62 @@ object EffectGiveItem : Effect<ItemStack>("give_item") {
     override fun onTrigger(config: Config, data: TriggerData, compileData: ItemStack): Boolean {
         val player = data.player ?: return false
 
-        DropQueue(player)
-            .addItem(compileData)
-            .forceTelekinesis()
-            .push()
+        when (val slotType = config.getString("slot").lowercase()) {
+            "hand" -> HandSlot.addToSlot(player, compileData)
+            "offhand" -> SlotSlot(40).addToSlot(player, compileData)
+            "helmet" -> SlotSlot(39).addToSlot(player, compileData)
+            "chestplate" -> SlotSlot(38).addToSlot(player, compileData)
+            "leggings" -> SlotSlot(37).addToSlot(player, compileData)
+            "boots" -> SlotSlot(36).addToSlot(player, compileData)
+            else -> {
+                val slot = slotType.toIntOrNull()
+
+                if (slot != null) {
+                    SlotSlot(slot).addToSlot(player, compileData)
+                } else {
+                    AnySlot.addToSlot(player, compileData)
+                }
+            }
+        }
 
         return true
     }
 
     override fun makeCompileData(config: Config, context: ViolationContext): ItemStack {
         return Items.lookup(config.getString("item")).item
+    }
+
+    private interface SlotType {
+        fun addToSlot(player: Player, item: ItemStack)
+    }
+
+    private object HandSlot : SlotType {
+        override fun addToSlot(player: Player, item: ItemStack) {
+            if (EmptyTestableItem().matches(player.inventory.itemInMainHand)) {
+                player.inventory.setItem(
+                    player.inventory.heldItemSlot, item
+                )
+            } else {
+                DropQueue(player).addItem(item).forceTelekinesis().push()
+            }
+        }
+    }
+
+    private class SlotSlot(val slot: Int) : SlotType {
+        override fun addToSlot(player: Player, item: ItemStack) {
+            if (EmptyTestableItem().matches(player.inventory.getItem(slot))) {
+                player.inventory.setItem(
+                    slot, item
+                )
+            } else {
+                DropQueue(player).addItem(item).forceTelekinesis().push()
+            }
+        }
+    }
+
+    private object AnySlot : SlotType {
+        override fun addToSlot(player: Player, item: ItemStack) {
+            DropQueue(player).addItem(item).forceTelekinesis().push()
+        }
     }
 }
