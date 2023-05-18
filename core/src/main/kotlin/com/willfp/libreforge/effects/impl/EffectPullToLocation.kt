f26583ea26c2647af9cc67ae5a7fdd249a4df99b
Auxilor <william.favierparsons1@gmail.com>
2023-05-18 18:00:28 +0100

    Added jump option to pull_to_location

--- core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectPullToLocation.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectPullToLocation.kt
@@ -5,8 +5,12 @@ import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.arguments
 import com.willfp.libreforge.effects.Effect
 import com.willfp.libreforge.getDoubleFromExpression
+import com.willfp.libreforge.normalize
+import com.willfp.libreforge.toFloat3
+import com.willfp.libreforge.toVector
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
+import dev.romainguy.kotlin.math.Float3
 
 object EffectPullToLocation : Effect<NoCompileData>("pull_to_location") {
     override val parameters = setOf(
@@ -26,8 +30,12 @@ object EffectPullToLocation : Effect<NoCompileData>("pull_to_location") {
             return false
         }
 
-        val vector = location.toVector().subtract(player.location.toVector()).normalize()
-            .multiply(config.getDoubleFromExpression("velocity", data))
+        val vector = location.toFloat3()
+            .minus(player.location.toFloat3())
+            .normalize()
+            .plus(Float3(0f, config.getDoubleFromExpression("jump", data).toFloat(), 0f))
+            .times(config.getDoubleFromExpression("velocity", data).toFloat())
+            .toVector()
 
         player.velocity = vector
 
