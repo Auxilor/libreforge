8949309cce3311305634f8c301b33ed2f8a934f4
Sen2000 <18487475+SenPr@users.noreply.github.com>
2023-08-15 18:37:17 +0700

    Add victim_movement_multiplier

---
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectVictimMovementMultiplier.kt
@@ -0,0 +1,36 @@
+package com.willfp.libreforge.effects.impl
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.NoCompileData
+import com.willfp.libreforge.arguments
+import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.plugin
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.attribute.Attribute
+
+object EffectVictimMovementMultiplier: Effect<NoCompileData>("victim_movement_multiplier") {
+    override val parameters = setOf(
+        TriggerParameter.VICTIM
+    )
+
+    override val arguments = arguments {
+        require("multiplier", "You must specify the multiplier!")
+    }
+
+    override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
+        val victim = data.victim ?: return false
+        val attribute = victim.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED) ?: return false
+        val attributeValue = attribute.value
+        val duration = if (config.has("duration")) config.getIntFromExpression("duration") else null
+
+        attribute.baseValue = attributeValue * config.getDoubleFromExpression("multiplier");
+        if (duration != null && duration > 0) {
+            plugin.scheduler.runLater(duration.toLong()) {
+                attribute.baseValue = attributeValue
+            }
+        }
+        return true
+    }
+
+}
