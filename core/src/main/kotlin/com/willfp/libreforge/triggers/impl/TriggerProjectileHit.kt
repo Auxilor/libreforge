70e88b83deb54cc02f0d5945bac4536b1c31ed8b
Auxilor <william.favierparsons1@gmail.com>
2023-08-19 15:27:19 +0100

    Fixed projectile_hit not working properly

--- core/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerProjectileHit.kt
+++ core/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerProjectileHit.kt
@@ -3,15 +3,20 @@ package com.willfp.libreforge.triggers.impl
 import com.willfp.libreforge.triggers.Trigger
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.entity.Arrow
+import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.ProjectileHitEvent
 
 object TriggerProjectileHit : Trigger("projectile_hit") {
     override val parameters = setOf(
         TriggerParameter.PLAYER,
+        TriggerParameter.VICTIM,
         TriggerParameter.PROJECTILE,
         TriggerParameter.LOCATION,
+        TriggerParameter.BLOCK,
         TriggerParameter.EVENT,
         TriggerParameter.VELOCITY
     )
@@ -31,8 +36,29 @@ object TriggerProjectileHit : Trigger("projectile_hit") {
                 player = shooter,
                 projectile = projectile,
                 location = projectile.location,
+                block = event.hitBlock,
                 event = event,
                 velocity = projectile.velocity
+            )
+        )
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: EntityDamageByEntityEvent) {
+        val arrow = event.damager as? Arrow ?: return
+        val victim = event.entity as? LivingEntity ?: return
+        val player = arrow.shooter as? Player ?: return
+
+        this.dispatch(
+            player,
+            TriggerData(
+                player = player,
+                victim = victim,
+                location = victim.location,
+                event = event,
+                velocity = arrow.velocity,
+                projectile = arrow,
+                value = event.finalDamage
             )
         )
     }
