af2ccac442269712382cac3969762b68169d899f
Auxilor <william.favierparsons1@gmail.com>
2023-08-30 10:18:41 +0100

    Imports

--- core/src/main/kotlin/com/willfp/libreforge/conditions/impl/ConditionHasItemData.kt
+++ core/src/main/kotlin/com/willfp/libreforge/conditions/impl/ConditionHasItemData.kt
@@ -1,18 +1,14 @@
 package com.willfp.libreforge.conditions.impl
 
 import com.willfp.eco.core.config.interfaces.Config
-import com.willfp.eco.core.placeholder.context.placeholderContext
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.ProvidedHolder
 import com.willfp.libreforge.arguments
 import com.willfp.libreforge.conditions.Condition
-import com.willfp.libreforge.getFormattedString
 import com.willfp.libreforge.getProvider
 import com.willfp.libreforge.itemData
-import com.willfp.libreforge.points
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
-import sun.jvm.hotspot.oops.CellTypeState.value
 
 object ConditionHasItemData : Condition<NoCompileData>("has_item_data") {
     override val arguments = arguments {
