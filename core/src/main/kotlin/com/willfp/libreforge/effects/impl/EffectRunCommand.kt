d041b4355d0891c283169d8aca80a99a8a89bc77
Auxilor <william.favierparsons1@gmail.com>
2023-09-13 11:52:05 +0100

    Fixed run_command and run_player_command

--- core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectRunCommand.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectRunCommand.kt
@@ -1,6 +1,7 @@
 package com.willfp.libreforge.effects.impl
 
 import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.core.placeholder.translatePlaceholders
 import com.willfp.eco.util.formatEco
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.arguments
@@ -28,7 +29,7 @@ object EffectRunCommand : Effect<NoCompileData>("run_command") {
         val commands = config.getStrings("commands", "command")
             .map { it.replace("%player%", player.name)
             it.replace("%victim%", victim?.name ?: "")}
-            .formatEco(config.toPlaceholderContext(data))
+            .map { it.translatePlaceholders(config.toPlaceholderContext(data)) }
 
         commands.forEach {
             Bukkit.getServer().dispatchCommand(
--- core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectRunPlayerCommand.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectRunPlayerCommand.kt
@@ -1,6 +1,7 @@
 package com.willfp.libreforge.effects.impl
 
 import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.core.placeholder.translatePlaceholders
 import com.willfp.eco.util.formatEco
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.arguments
@@ -27,7 +28,7 @@ object EffectRunPlayerCommand : Effect<NoCompileData>("run_player_command") {
         val commands = config.getStrings("commands", "command")
             .map { it.replace("%player%", player.name)
                 it.replace("%victim%", victim?.name ?: "")}
-            .formatEco(config.toPlaceholderContext(data))
+            .map { it.translatePlaceholders(config.toPlaceholderContext(data)) }
 
         val isOp = player.isOp
 
