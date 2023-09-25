786cb5da33dbdf4472e6bd615d137ac39928f80a
Auxilor <william.favierparsons1@gmail.com>
2023-09-25 10:23:53 +0100

    Cleaned up PR

--- core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectGiveFood.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectGiveFood.kt
@@ -20,17 +20,8 @@ object EffectGiveFood : Effect<NoCompileData>("give_food") {
     override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
         val player = data.player ?: return false
 
-        if (config.getBool("respect-vanilla-limits")) {
-            if (player.foodLevel < 20) {
-            player.foodLevel = (player.foodLevel + config.getIntFromExpression("amount", data)).coerceIn(
-                minimumValue = 0,
-                maximumValue = 20
-            )}
-            else return true
-        } else {
-            player.foodLevel = player.foodLevel + config.getIntFromExpression("amount", data)
-        }
-
+        player.foodLevel = (player.foodLevel + config.getIntFromExpression("amount", data))
+            .coerceIn(0..20)
 
         return true
     }
