18391a6c0af82a2697733b5618eb52fb87f2d56d
Auxilor <william.favierparsons1@gmail.com>
2023-10-19 12:42:25 +0100

    Rewrote is_behind_Victim

--- core/src/main/kotlin/com/willfp/libreforge/filters/impl/FilterIsBehindVictim.kt
+++ core/src/main/kotlin/com/willfp/libreforge/filters/impl/FilterIsBehindVictim.kt
@@ -3,6 +3,7 @@ package com.willfp.libreforge.filters.impl
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.angle
+import com.willfp.libreforge.dot
 import com.willfp.libreforge.filters.Filter
 import com.willfp.libreforge.toFloat3
 import com.willfp.libreforge.triggers.TriggerData
@@ -21,8 +22,7 @@ object FilterIsBehindVictim : Filter<NoCompileData, Boolean>("is_behind_victim")
         val playerVector = player.location.direction.toFloat3().xz
         val victimVector = victim.location.direction.toFloat3().xz
 
-        val angle = Math.toDegrees(playerVector.angle(victimVector).toDouble()).roundToInt()
-        val isBehind = angle in (-32..60) // Old code from Backstab from EE v8
+        val isBehind = playerVector.dot(victimVector) < 0
         return isBehind == value
     }
 }
