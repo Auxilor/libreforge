7987e1c6eefb2c5ac4c7a742fe1a7f57f3a1fa31
Auxilor <william.favierparsons1@gmail.com>
2023-11-11 15:06:53 +0000

    Added cooldown_effects

--- core/src/main/kotlin/com/willfp/libreforge/effects/arguments/impl/ArgumentCooldown.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/arguments/impl/ArgumentCooldown.kt
@@ -1,10 +1,15 @@
 package com.willfp.libreforge.effects.arguments.impl
 
+import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.eco.util.PlayerUtils
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.formatEco
 import com.willfp.libreforge.ConfigurableElement
+import com.willfp.libreforge.NamedValue
 import com.willfp.libreforge.NoCompileData
+import com.willfp.libreforge.ViolationContext
+import com.willfp.libreforge.effects.Chain
+import com.willfp.libreforge.effects.Effects
 import com.willfp.libreforge.effects.arguments.EffectArgument
 import com.willfp.libreforge.getDoubleFromExpression
 import com.willfp.libreforge.plugin
@@ -13,11 +18,11 @@ import org.bukkit.Sound
 import java.util.UUID
 import kotlin.math.ceil
 
-object ArgumentCooldown : EffectArgument<NoCompileData>("cooldown") {
+object ArgumentCooldown : EffectArgument<Chain?>("cooldown") {
     // Maps ConfiguredEffect UUIDs to Player UUIDs mapped to expiry time
     private val cooldownTracker = mutableMapOf<UUID, MutableMap<UUID, Long>>()
 
-    override fun isMet(element: ConfigurableElement, trigger: DispatchedTrigger, compileData: NoCompileData): Boolean {
+    override fun isMet(element: ConfigurableElement, trigger: DispatchedTrigger, compileData: Chain?): Boolean {
         return getCooldown(element, trigger) <= 0
     }
 
@@ -30,14 +35,20 @@ object ArgumentCooldown : EffectArgument<NoCompileData>("cooldown") {
         return secondsLeft.toInt()
     }
 
-    override fun ifMet(element: ConfigurableElement, trigger: DispatchedTrigger, compileData: NoCompileData) {
+    override fun ifMet(element: ConfigurableElement, trigger: DispatchedTrigger, compileData: Chain?) {
         val effectEndTimes = cooldownTracker[element.uuid] ?: mutableMapOf()
         effectEndTimes[trigger.player.uniqueId] = System.currentTimeMillis() +
                 (element.config.getDoubleFromExpression("cooldown", trigger.data) * 1000L).toLong()
         cooldownTracker[element.uuid] = effectEndTimes
     }
 
-    override fun ifNotMet(element: ConfigurableElement, trigger: DispatchedTrigger, compileData: NoCompileData) {
+    override fun ifNotMet(element: ConfigurableElement, trigger: DispatchedTrigger, compileData: Chain?) {
+        val cooldown = getCooldown(element, trigger)
+
+        compileData?.trigger(trigger.copy().apply {
+            addPlaceholder(NamedValue("seconds", cooldown))
+        })
+
         if (element.config.getBoolOrNull("send_cooldown_message") == false) {
             return
         }
@@ -47,8 +58,6 @@ object ArgumentCooldown : EffectArgument<NoCompileData>("cooldown") {
         }
 
         val player = trigger.player
-
-        val cooldown = getCooldown(element, trigger)
 
         val message = element.config.getStringOrNull("cooldown_message")
             ?.replace("%seconds%", cooldown.toString())
@@ -71,5 +80,12 @@ object ArgumentCooldown : EffectArgument<NoCompileData>("cooldown") {
                 plugin.configYml.getDouble("cooldown.sound.pitch").toFloat()
             )
         }
+    }
+
+    override fun makeCompileData(config: Config, context: ViolationContext): Chain? {
+        return Effects.compileChain(
+            config.getSubsections("cooldown_effects"),
+            context.with("cooldown_effects")
+        )
     }
 }
