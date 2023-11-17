3c27172eb728c99c45bc27b1e4a2c749444d28ee
Auxilor <william.favierparsons1@gmail.com>
2023-11-17 18:52:56 +0000

    Added is_expression_true filter

---
+++ core/src/main/kotlin/com/willfp/libreforge/filters/impl/FilterIsExpressionTrue.kt
@@ -0,0 +1,24 @@
+package com.willfp.libreforge.filters.impl
+
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.util.evaluateExpression
+import com.willfp.libreforge.ViolationContext
+import com.willfp.libreforge.filters.Filter
+import com.willfp.libreforge.toPlaceholderContext
+import com.willfp.libreforge.triggers.TriggerData
+
+object FilterIsExpressionTrue : Filter<Config, String>("is_expression_true") {
+    override fun getValue(config: Config, data: TriggerData?, key: String): String {
+        return config.getString(key)
+    }
+
+    override fun isMet(data: TriggerData, value: String, compileData: Config): Boolean {
+        val victim = data.victim ?: return true
+
+        return evaluateExpression(value, compileData.toPlaceholderContext(data)) > 0
+    }
+
+    override fun makeCompileData(config: Config, context: ViolationContext, values: String): Config {
+        return config
+    }
+}
