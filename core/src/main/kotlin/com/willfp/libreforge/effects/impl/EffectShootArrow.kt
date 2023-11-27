c623150d71436ea7e97b80a44316ec1615662cd6
PQguanfang <pqguanfang@foxmail.com>
2023-11-27 08:25:46 +0800

    Added effect arg

--- core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectShootArrow.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectShootArrow.kt
@@ -5,12 +5,15 @@ import com.willfp.eco.util.runExempted
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.effects.Effect
 import com.willfp.libreforge.getDoubleFromExpression
+import com.willfp.libreforge.getIntFromExpression
 import com.willfp.libreforge.getOrNull
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
 import org.bukkit.entity.AbstractArrow
 import org.bukkit.entity.Arrow
 import org.bukkit.event.entity.EntityShootBowEvent
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 
 object EffectShootArrow : Effect<NoCompileData>("shoot_arrow") {
     override val parameters = setOf(
@@ -41,6 +44,20 @@ object EffectShootArrow : Effect<NoCompileData>("shoot_arrow") {
 
             if (config.getBool("no_source")) {
                 arrow.shooter = null
+            }
+
+            if (config.getStringOrNull("effect") != null) {
+                arrow.addCustomEffect(
+                    PotionEffect(
+                        PotionEffectType.getByName(config.getString("effect").uppercase())
+                            ?: PotionEffectType.INCREASE_DAMAGE,
+                        config.getIntFromExpression("duration", data),
+                        config.getIntFromExpression("level", data) - 1,
+                        true,
+                        true,
+                        true
+                    ), false
+                )
             }
         }
 
