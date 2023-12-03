ebda2fe5241b4b4dc3d5fbf26c469c127c34cf84
_OfTeN_ <dkoften@gmail.com>
2023-12-03 05:46:35 +0300

    Changed smelt trigger to work without having GUI open

--- core/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerSmelt.kt
+++ core/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerSmelt.kt
@@ -1,25 +1,48 @@
 package com.willfp.libreforge.triggers.impl
 
+import com.willfp.eco.core.gui.player
+import com.willfp.libreforge.plugin
 import com.willfp.libreforge.toDispatcher
 import com.willfp.libreforge.triggers.Trigger
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
+import org.bukkit.Location
 import org.bukkit.block.Furnace
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.inventory.FurnaceSmeltEvent
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.FurnaceInventory
 
 object TriggerSmelt : Trigger("smelt") {
+    private val playerCache = mutableMapOf<Location, Player>()
+
     override val parameters = setOf(
         TriggerParameter.PLAYER,
         TriggerParameter.LOCATION,
         TriggerParameter.ITEM
     )
 
+    @EventHandler
+    fun handle(event: InventoryClickEvent) {
+        val inventory = event.player.openInventory.topInventory as? FurnaceInventory ?: return
+        val player = event.player
+        val location = inventory.location ?: return
+        val oldContents = inventory.contents
+
+        plugin.scheduler.runLater(2) {
+            val newContents = inventory.contents
+
+            if (!oldContents.contentEquals(newContents)) {
+                playerCache[location] = player
+            }
+        }
+    }
+
     @EventHandler(ignoreCancelled = true)
     fun handle(event: FurnaceSmeltEvent) {
         val furnace = event.block.state as? Furnace ?: return
-        val player = furnace.inventory.viewers.firstOrNull() as? Player ?: return
+        val player = playerCache[event.block.location] ?: return
         val item = event.result
 
         this.dispatch(
