00e8cfe6545699691ad81ad597f054b2f086ff59
Auxilor <william.favierparsons1@gmail.com>
2024-01-01 19:49:03 +0000

    Added multi-line message support to broadcast effect

--- core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectBroadcast.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectBroadcast.kt
@@ -5,6 +5,7 @@ import com.willfp.eco.util.formatEco
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.arguments
 import com.willfp.libreforge.effects.Effect
+import com.willfp.libreforge.getStrings
 import com.willfp.libreforge.toPlaceholderContext
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
@@ -19,12 +20,14 @@ object EffectBroadcast : Effect<NoCompileData>("broadcast") {
     }
 
     override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
-        val message = config.getString("message")
-            .replace("%player%", data.player?.name ?: "%player%")
+        val messages = config.getStrings("messages", "message")
+            .map { it.replace("%player%", data.player?.name ?: "%player%") }
             .formatEco(config.toPlaceholderContext(data))
 
-        @Suppress("DEPRECATION")
-        Bukkit.getServer().broadcastMessage(message)
+        for (message in messages) {
+            @Suppress("DEPRECATION")
+            Bukkit.getServer().broadcastMessage(message)
+        }
 
         return true
     }
