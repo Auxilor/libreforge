c16e5fd8dc3f3f88e797e97f6be61f86c1ecf47a
Will FP <william.favierparsons1@gmail.com>
2024-02-05 20:57:15 +0000

    Fixed placeholders not being formatted in not-met-lines

--- core/src/main/kotlin/com/willfp/libreforge/conditions/ConditionList.kt
+++ core/src/main/kotlin/com/willfp/libreforge/conditions/ConditionList.kt
@@ -3,9 +3,13 @@ package com.willfp.libreforge.conditions
 import com.willfp.eco.util.formatEco
 import com.willfp.libreforge.DelegatedList
 import com.willfp.libreforge.Dispatcher
+import com.willfp.libreforge.EmptyProvidedHolder.holder
+import com.willfp.libreforge.GlobalDispatcher.dispatcher
 import com.willfp.libreforge.ProvidedHolder
+import com.willfp.libreforge.applyHolder
 import com.willfp.libreforge.get
 import com.willfp.libreforge.toDispatcher
+import com.willfp.libreforge.toPlaceholderContext
 import com.willfp.libreforge.triggers.DispatchedTrigger
 import org.bukkit.entity.Player
 
@@ -44,12 +48,28 @@ class ConditionList(
     /**
      * Get all not met lines.
      */
-    fun getNotMetLines(dispatcher: Dispatcher<*>, holder: ProvidedHolder): List<String> =
-        this.filter { it.notMetLines.isNotEmpty() }
-            .filter { !it.isMet(dispatcher, holder) }
-            .flatMap { it.notMetLines }
-            .map { it.formatEco(dispatcher.get(), formatPlaceholders = true) }
+    fun getNotMetLines(dispatcher: Dispatcher<*>, holder: ProvidedHolder): List<String> {
+        val lines = mutableListOf<String>()
 
+        for (block in this) {
+            if (block.notMetLines.isEmpty()) {
+                continue
+            }
+
+            if (block.isMet(dispatcher, holder)) {
+                continue
+            }
+
+            val conditionLines = block.notMetLines
+            val context = block.config.applyHolder(holder, dispatcher).toPlaceholderContext()
+
+            lines += conditionLines.map {
+                it.formatEco(context)
+            }
+        }
+
+        return lines
+    }
 
     @Deprecated(
         "Use areMet(dispatcher, holder) instead.",
