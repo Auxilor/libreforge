ddad3cdea0f65ded98e67e81e5e804d6f7f0b24f
Will FP <william.favierparsons1@gmail.com>
2024-02-15 12:53:07 +0000

    Fixed add_holder_in_radius, fixed add_holder_to_victim memory leak

--- core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectAddHolderInRadius.kt
+++ core/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectAddHolderInRadius.kt
@@ -97,9 +97,9 @@ object EffectAddHolderInRadius : Effect<HolderTemplate>("add_holder_in_radius") 
         val applyToSelf: Boolean
     ) {
         fun canApplyTo(dispatcher: Dispatcher<*>): Boolean {
-            val location = dispatcher.location ?: return false
+            val otherLocation = dispatcher.location ?: return false
 
-            if (location.world != location.world) {
+            if (location.world != otherLocation.world) {
                 return false
             }
 
@@ -107,7 +107,7 @@ object EffectAddHolderInRadius : Effect<HolderTemplate>("add_holder_in_radius") 
                 return false
             }
 
-            if (location.toVector().distanceSquared(location.toVector()) > radius * radius) {
+            if (location.toVector().distanceSquared(otherLocation.toVector()) > radius * radius) {
                 return false
             }
 
