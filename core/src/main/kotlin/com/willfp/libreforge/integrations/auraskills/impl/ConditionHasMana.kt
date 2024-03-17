c38467ffe809344c530e199963b0b48ff9df691c
Archy-X <archydevelopment@gmail.com>
2024-03-17 12:23:05 -0400

    Add AuraSkills 2.0 integration

---
+++ core/src/main/kotlin/com/willfp/libreforge/integrations/auraskills/impl/ConditionHasMana.kt
@@ -0,0 +1,32 @@
+package com.willfp.libreforge.integrations.auraskills.impl
+
+import com.archyx.aureliumskills.api.event.ManaRegenerateEvent
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.*
+import com.willfp.libreforge.conditions.Condition
+import dev.aurelium.auraskills.api.AuraSkillsApi
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+
+object ConditionHasMana : Condition<NoCompileData>("has_mana") {
+    override val arguments = arguments {
+        require("amount", "You must specify the amount of mana!")
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    fun handle(event: ManaRegenerateEvent) {
+        event.player.toDispatcher().updateEffects()
+    }
+
+    override fun isMet(
+        dispatcher: Dispatcher<*>,
+        config: Config,
+        holder: ProvidedHolder,
+        compileData: NoCompileData
+    ): Boolean {
+        val player = dispatcher.get<Player>() ?: return false
+
+        return AuraSkillsApi.get().getUser(player.uniqueId).mana > config.getDoubleFromExpression("amount", player)
+    }
+}
