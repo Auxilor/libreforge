cad74712db264918cb081c0951ec9a79ca5d19a3
Auxilor <william.favierparsons1@gmail.com>
2024-07-25 09:55:57 +0100

    Cleaned up spawner_entity

--- core/common/src/main/kotlin/com/willfp/libreforge/filters/impl/FilterSpawnerEntity.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/filters/impl/FilterSpawnerEntity.kt
@@ -8,21 +8,16 @@ import com.willfp.libreforge.triggers.TriggerData
 import org.bukkit.block.CreatureSpawner
 
 object FilterSpawnerEntity : Filter<NoCompileData, Collection<String>>("spawner_entity") {
-
     override fun getValue(config: Config, data: TriggerData?, key: String): Collection<String> {
         return config.getStrings(key)
     }
 
     override fun isMet(data: TriggerData, value: Collection<String>, compileData: NoCompileData): Boolean {
-        val block = data.block ?: return false
+        val block = data.block ?: return true
+        val state = block.state as? CreatureSpawner ?: return false
 
-        if (block.state is CreatureSpawner) {
-            val spawner = block.state as CreatureSpawner
-            val spawnerEntityType = spawner.spawnedType?.name ?: "null"
+        val spawnerEntityType = state.spawnedType?.name ?: "null"
 
-            return value.containsIgnoreCase(spawnerEntityType)
-        }
-
-        return false
+        return value.containsIgnoreCase(spawnerEntityType)
     }
 }
