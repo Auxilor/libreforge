14a25cbb9340b26843d27bc1c95be79c1df3fefb
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2024-09-21 17:39:21 +0100

    reverted back to `sheep_color` to avoid confusion

--- core/common/src/main/kotlin/com/willfp/libreforge/filters/impl/FilterSheepColor.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/filters/impl/FilterSheepColor.kt
@@ -6,15 +6,15 @@ import com.willfp.libreforge.filters.Filter
 import com.willfp.libreforge.triggers.TriggerData
 import org.bukkit.entity.Sheep
 
-object FilterEntityColor : Filter<NoCompileData, Collection<String>>("entity_color") {
+object FilterSheepColor : Filter<NoCompileData, Collection<String>>("sheep_color") {
     override fun getValue(config: Config, data: TriggerData?, key: String): Collection<String> {
         return config.getStrings(key)
     }
 
     override fun isMet(data: TriggerData, value: Collection<String>, compileData: NoCompileData): Boolean {
         val entity = data.victim as? Sheep ?: return false
-        val entityColor = entity.color?.name
+        val sheepColor = entity.color?.name
 
-        return value.any { it.equals(entityColor, ignoreCase = true) }
+        return value.any { it.equals(sheepColor, ignoreCase = true) }
     }
 }
