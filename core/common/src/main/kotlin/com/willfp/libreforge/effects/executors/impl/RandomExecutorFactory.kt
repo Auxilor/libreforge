7538d64894715616f023a258f0014fda4d502e19
Nikolai Connolly <connollynh@gmail.com>
2024-11-17 16:52:55 -0600

    ty often

--- core/common/src/main/kotlin/com/willfp/libreforge/effects/executors/impl/RandomExecutorFactory.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/effects/executors/impl/RandomExecutorFactory.kt
@@ -4,6 +4,7 @@ import com.willfp.libreforge.effects.Chain
 import com.willfp.libreforge.effects.ChainElement
 import com.willfp.libreforge.effects.executors.ChainExecutor
 import com.willfp.libreforge.effects.executors.ChainExecutorFactory
+import com.willfp.libreforge.toWeightedList
 import com.willfp.libreforge.triggers.DispatchedTrigger
 import org.bukkit.Bukkit
 import java.util.Random
@@ -16,26 +17,11 @@ object RandomExecutorFactory : ChainExecutorFactory("random") {
 
     object RandomChainExecutor : ChainExecutor {
         override fun execute(chain: Chain, trigger: DispatchedTrigger): Boolean {
-            if (chain.isEmpty()) return false
-
-            if (chain.size == 1) {
-                return chain.first().trigger(trigger)
+            val element = chain.toWeightedList().randomOrNull()
+            if (element != null) {
+                if (element.trigger(trigger)) return true
             }
-
-            val randomMap: TreeMap<Double, ChainElement<*>> = TreeMap()
-            chain.forEach {
-                var weight = it.weight
-
-                if (weight < 0) weight = 0.0
-                if (weight != 0.0) randomMap[weight] = it
-            }
-
-            if (randomMap.lastKey() == 0.0) { // If all weights are 0
-                return chain.random().trigger(trigger)
-            }
-
-            val randomValue = random.nextDouble() * randomMap.lastKey()
-            return randomMap.higherEntry(randomValue)?.value?.trigger(trigger) ?: false
+            return false
         }
     }
 }
