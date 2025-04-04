3996f552adc70052279525222cff5af51cff2972
ChimpGamer <Tim.Cremers@Ziggo.nl>
2025-04-04 15:36:13 +0200

    Changed mob_coins_multiplier to mob_coins_drop_multiplier and added mob_coins_chance_multiplier

--- core/common/src/main/kotlin/com/willfp/libreforge/integrations/ultimatemobcoins/UltimateMobCoinsIntegration.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/integrations/ultimatemobcoins/UltimateMobCoinsIntegration.kt
@@ -3,11 +3,13 @@ package com.willfp.libreforge.integrations.ultimatemobcoins
 import com.willfp.eco.core.EcoPlugin
 import com.willfp.libreforge.effects.Effects
 import com.willfp.libreforge.integrations.LoadableIntegration
-import com.willfp.libreforge.integrations.ultimatemobcoins.impl.EffectMobCoinsMultiplier
+import com.willfp.libreforge.integrations.ultimatemobcoins.impl.EffectMobCoinsChanceMultiplier
+import com.willfp.libreforge.integrations.ultimatemobcoins.impl.EffectMobCoinsDropMultiplier
 
 object UltimateMobCoinsIntegration: LoadableIntegration {
     override fun load(plugin: EcoPlugin) {
-        Effects.register(EffectMobCoinsMultiplier)
+        Effects.register(EffectMobCoinsDropMultiplier)
+        Effects.register(EffectMobCoinsChanceMultiplier)
     }
 
     override fun getPluginName(): String = "UltimateMobCoins"
--- core/common/src/main/kotlin/com/willfp/libreforge/integrations/ultimatemobcoins/impl/EffectMobCoinsChanceMultiplier.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/integrations/ultimatemobcoins/impl/EffectMobCoinsChanceMultiplier.kt
@@ -2,14 +2,14 @@ package com.willfp.libreforge.integrations.ultimatemobcoins.impl
 
 import com.willfp.libreforge.effects.templates.MultiplierEffect
 import com.willfp.libreforge.toDispatcher
-import nl.chimpgamer.ultimatemobcoins.paper.events.MobCoinsReceiveEvent
+import nl.chimpgamer.ultimatemobcoins.paper.events.PrepareMobCoinDropEvent
 import org.bukkit.event.EventHandler
 
-object EffectMobCoinsMultiplier : MultiplierEffect("mob_coins_multiplier") {
+object EffectMobCoinsChanceMultiplier : MultiplierEffect("mob_coins_chance_multiplier") {
     @EventHandler(ignoreCancelled = true)
-    fun handle(event: MobCoinsReceiveEvent) {
+    fun handle(event: PrepareMobCoinDropEvent) {
         val player = event.player
 
-        event.amount = event.amount.multiply(getMultiplier(player.toDispatcher()).toBigDecimal())
+        event.mobCoin.chance *= getMultiplier(player.toDispatcher())
     }
 }
