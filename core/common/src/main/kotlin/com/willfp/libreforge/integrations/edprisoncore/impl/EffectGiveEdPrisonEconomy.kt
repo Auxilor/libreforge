ca802033435950d43300e4c256518134c4e8923f
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2025-07-26 12:52:07 +0100

    Add currency validation to EdPrison economy effects
    Updated EffectGiveEdPrisonEconomy and EffectSetEdPrisonEconomy to validate the specified currency against available economies during compile data creation. This prevents invalid currency types from being used and logs configuration violations if necessary.

--- core/common/src/main/kotlin/com/willfp/libreforge/integrations/edprisoncore/impl/EffectGiveEdPrisonEconomy.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/integrations/edprisoncore/impl/EffectGiveEdPrisonEconomy.kt
@@ -2,14 +2,16 @@ package com.willfp.libreforge.integrations.edprisoncore.impl
 
 import com.edwardbelt.edprison.utils.EconomyUtils
 import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.ConfigViolation
 import com.willfp.libreforge.NoCompileData
+import com.willfp.libreforge.ViolationContext
 import com.willfp.libreforge.arguments
 import com.willfp.libreforge.effects.Effect
 import com.willfp.libreforge.getDoubleFromExpression
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
 
-object EffectGiveEdPrisonEconomy : Effect<NoCompileData>("give_edprison_economy") {
+object EffectGiveEdPrisonEconomy : Effect<String?>("give_edprison_economy") {
     override val parameters = setOf(
         TriggerParameter.PLAYER
     )
@@ -19,15 +21,30 @@ object EffectGiveEdPrisonEconomy : Effect<NoCompileData>("give_edprison_economy"
         require("amount", "You must specify the amount of economy to give!")
     }
 
-    override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
+    override fun onTrigger(config: Config, data: TriggerData, compileData: String?): Boolean {
         val player = data.player ?: return false
         val uuid = player.uniqueId
 
-        val currency = config.getString("type") ?: return false
+        val currency = config.getString("type")
         val amount = config.getDoubleFromExpression("amount", data)
 
         EconomyUtils.addEconomy(uuid, currency, amount)
 
         return true
+    }
+
+    override fun makeCompileData(config: Config, context: ViolationContext): String? {
+        val currency = config.getString("type")
+        val validCurrencies = EconomyUtils.getAllEconomies()
+
+        if (currency !in validCurrencies) {
+            context.log(
+                this,
+                ConfigViolation("type", "You must specify the type of economy to give!")
+            )
+            return null
+        }
+
+        return currency
     }
 }
--- core/common/src/main/kotlin/com/willfp/libreforge/integrations/edprisoncore/impl/EffectSetEdPrisonEconomy.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/integrations/edprisoncore/impl/EffectSetEdPrisonEconomy.kt
@@ -2,14 +2,15 @@ package com.willfp.libreforge.integrations.edprisoncore.impl
 
 import com.edwardbelt.edprison.utils.EconomyUtils
 import com.willfp.eco.core.config.interfaces.Config
-import com.willfp.libreforge.NoCompileData
+import com.willfp.libreforge.ConfigViolation
+import com.willfp.libreforge.ViolationContext
 import com.willfp.libreforge.arguments
 import com.willfp.libreforge.effects.Effect
 import com.willfp.libreforge.getDoubleFromExpression
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
 
-object EffectSetEdPrisonEconomy  : Effect<NoCompileData>("set_edprison_economy") {
+object EffectSetEdPrisonEconomy : Effect<String?>("set_edprison_economy") {
     override val parameters = setOf(
         TriggerParameter.PLAYER
     )
@@ -19,15 +20,29 @@ object EffectSetEdPrisonEconomy  : Effect<NoCompileData>("set_edprison_economy")
         require("amount", "You must specify the amount of economy to give!")
     }
 
-    override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
+    override fun onTrigger(config: Config, data: TriggerData, compileData: String?): Boolean {
         val player = data.player ?: return false
         val uuid = player.uniqueId
-
-        val currency = config.getString("type")
+        val currency = compileData ?: return false
         val amount = config.getDoubleFromExpression("amount", data)
 
         EconomyUtils.setEco(uuid, currency, amount)
 
         return true
+    }
+
+    override fun makeCompileData(config: Config, context: ViolationContext): String? {
+        val currency = config.getString("type")
+        val validCurrencies = EconomyUtils.getAllEconomies()
+
+        if (currency !in validCurrencies) {
+            context.log(
+                this,
+                ConfigViolation("type", "You must specify the type of economy to give!")
+            )
+            return null
+        }
+
+        return currency
     }
 }
