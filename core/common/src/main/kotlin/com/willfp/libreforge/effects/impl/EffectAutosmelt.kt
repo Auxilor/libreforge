ca7b8d542b88c0b5b41cba94ba4509a8d7e7ffde
Koudyasek <58906505+Koudyasek@users.noreply.github.com>
2025-08-17 15:14:29 +0200

    Fixed autosmelt + potion_cloud effect

--- core/common/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectAutosmelt.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectAutosmelt.kt
@@ -1,7 +1,6 @@
 package com.willfp.libreforge.effects.impl
 
 import com.willfp.eco.core.config.interfaces.Config
-import com.willfp.eco.core.fast.FastItemStack
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.arguments
 import com.willfp.libreforge.effects.Effect
@@ -12,7 +11,6 @@ import com.willfp.libreforge.triggers.event.EditableBlockDropEvent
 import com.willfp.libreforge.triggers.event.EditableDropEvent
 import org.bukkit.Bukkit
 import org.bukkit.Material
-import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
 import org.bukkit.inventory.FurnaceRecipe
 import org.bukkit.inventory.ItemStack
@@ -35,6 +33,13 @@ object EffectAutosmelt : Effect<NoCompileData>("autosmelt") {
     )
 
     init {
+        loadRecipes()
+    }
+
+    private fun loadRecipes() {
+        recipes.clear()
+        
+        // Load recipes from Bukkit
         val iterator = Bukkit.recipeIterator()
         while (iterator.hasNext()) {
             val recipe = iterator.next()
@@ -44,6 +49,32 @@ object EffectAutosmelt : Effect<NoCompileData>("autosmelt") {
             val xp = ceil(recipe.experience).toInt()
             @Suppress("DEPRECATION")
             recipes[recipe.input.type] = Pair(recipe.result.type, xp)
+        }
+
+        // Add fallback recipes for common ores if they're missing (using vanilla XP values)
+        addFallbackRecipe(Material.IRON_ORE, Material.IRON_INGOT, ceil(0.7f).toInt())
+        addFallbackRecipe(Material.DEEPSLATE_IRON_ORE, Material.IRON_INGOT, ceil(0.7f).toInt())
+        addFallbackRecipe(Material.GOLD_ORE, Material.GOLD_INGOT, ceil(1.0f).toInt())
+        addFallbackRecipe(Material.DEEPSLATE_GOLD_ORE, Material.GOLD_INGOT, ceil(1.0f).toInt())
+        addFallbackRecipe(Material.COPPER_ORE, Material.COPPER_INGOT, ceil(0.7f).toInt())
+        addFallbackRecipe(Material.DEEPSLATE_COPPER_ORE, Material.COPPER_INGOT, ceil(0.7f).toInt())
+        addFallbackRecipe(Material.COAL_ORE, Material.COAL, ceil(0.1f).toInt())
+        addFallbackRecipe(Material.DEEPSLATE_COAL_ORE, Material.COAL, ceil(0.1f).toInt())
+        addFallbackRecipe(Material.DIAMOND_ORE, Material.DIAMOND, ceil(1.0f).toInt())
+        addFallbackRecipe(Material.DEEPSLATE_DIAMOND_ORE, Material.DIAMOND, ceil(1.0f).toInt())
+        addFallbackRecipe(Material.EMERALD_ORE, Material.EMERALD, ceil(1.0f).toInt())
+        addFallbackRecipe(Material.DEEPSLATE_EMERALD_ORE, Material.EMERALD, ceil(1.0f).toInt())
+        addFallbackRecipe(Material.LAPIS_ORE, Material.LAPIS_LAZULI, ceil(0.2f).toInt())
+        addFallbackRecipe(Material.DEEPSLATE_LAPIS_ORE, Material.LAPIS_LAZULI, ceil(0.2f).toInt())
+        addFallbackRecipe(Material.REDSTONE_ORE, Material.REDSTONE, ceil(0.7f).toInt())
+        addFallbackRecipe(Material.DEEPSLATE_REDSTONE_ORE, Material.REDSTONE, ceil(0.7f).toInt())
+        addFallbackRecipe(Material.NETHER_GOLD_ORE, Material.GOLD_NUGGET, ceil(1.0f).toInt())
+        addFallbackRecipe(Material.NETHER_QUARTZ_ORE, Material.QUARTZ, ceil(0.2f).toInt())
+    }
+
+    private fun addFallbackRecipe(input: Material, output: Material, xp: Int) {
+        if (!recipes.containsKey(input)) {
+            recipes[input] = Pair(output, xp)
         }
     }
 
@@ -70,8 +101,16 @@ object EffectAutosmelt : Effect<NoCompileData>("autosmelt") {
     }
 
     private fun handleEvent(player: Player, event: EditableDropEvent, config: Config) {
-        val fortune = FastItemStack.wrap(player.inventory.itemInMainHand)
-            .getEnchantmentLevel(getEnchantment("fortune")!!, false)
+        val fortune = try {
+            val fortuneEnchant = getEnchantment("fortune")
+            if (fortuneEnchant != null) {
+                player.inventory.itemInMainHand.getEnchantmentLevel(fortuneEnchant)
+            } else {
+                0
+            }
+        } catch (_: Exception) {
+            0
+        }
 
         event.addModifier {
             var (type, xp) = getOutput(it.type)
