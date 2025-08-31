adfc0773449a45ec0a5fa5e34589731c9e6faf22
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2025-08-31 10:47:36 +0100

    Added MythicMobs support to DamageMultiplier and TakeEntityDamage

--- core/common/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectDamageMultiplier.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/effects/impl/EffectDamageMultiplier.kt
@@ -7,6 +7,7 @@ import com.willfp.libreforge.effects.Effect
 import com.willfp.libreforge.getDoubleFromExpression
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
+import io.lumine.mythic.bukkit.events.MythicDamageEvent
 import org.bukkit.event.entity.EntityDamageEvent
 
 object EffectDamageMultiplier : Effect<NoCompileData>("damage_multiplier") {
@@ -21,9 +22,19 @@ object EffectDamageMultiplier : Effect<NoCompileData>("damage_multiplier") {
     }
 
     override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
-        val event = data.event as? EntityDamageEvent ?: return false
-        event.damage *= config.getDoubleFromExpression("multiplier", data)
+        val multiplier = config.getDoubleFromExpression("multiplier", data)
 
-        return true
+        when (val event = data.event) {
+            is EntityDamageEvent -> {
+                event.damage *= multiplier
+                return true
+            }
+            is MythicDamageEvent -> {
+                event.damage *= multiplier
+                return true
+            }
+        }
+
+        return false
     }
 }
--- core/common/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerTakeEntityDamage.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerTakeEntityDamage.kt
@@ -5,6 +5,9 @@ import com.willfp.libreforge.triggers.Trigger
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
 import com.willfp.libreforge.triggers.tryAsLivingEntity
+import io.lumine.mythic.bukkit.MythicBukkit
+import io.lumine.mythic.bukkit.events.MythicDamageEvent
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.EntityDamageByEntityEvent
@@ -24,6 +27,12 @@ object TriggerTakeEntityDamage : Trigger("take_entity_damage") {
 
         val victim = event.entity
 
+        if (Bukkit.getPluginManager().isPluginEnabled("MythicMobs")) {
+            if (MythicBukkit.inst().mobManager.isMythicMob(attacker)) {
+                return
+            }
+        }
+
         if (event.cause == EntityDamageEvent.DamageCause.THORNS) {
             return
         }
@@ -36,6 +45,23 @@ object TriggerTakeEntityDamage : Trigger("take_entity_damage") {
                 location = attacker.location,
                 event = event,
                 value = event.finalDamage
+            )
+        )
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: MythicDamageEvent) {
+        val caster = event.caster?.entity?.bukkitEntity?.tryAsLivingEntity() ?: return
+        val victim = event.target?.bukkitEntity ?: return
+
+        this.dispatch(
+            victim.toDispatcher(),
+            TriggerData(
+                player = victim as? Player,
+                victim = caster,
+                location = caster.location,
+                event = event,
+                value = event.damage
             )
         )
     }
