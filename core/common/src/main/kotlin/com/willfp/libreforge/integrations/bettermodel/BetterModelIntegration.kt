8c20383d87b6160eeef8ef0594b1e43461dd6bb2
NicoNeko <nicuch100@gmail.com>
2026-01-07 09:24:03 +0200

    Add stop_animation, update play_animation

--- core/common/src/main/kotlin/com/willfp/libreforge/integrations/bettermodel/BetterModelIntegration.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/integrations/bettermodel/BetterModelIntegration.kt
@@ -4,10 +4,12 @@ import com.willfp.eco.core.EcoPlugin
 import com.willfp.libreforge.effects.Effects
 import com.willfp.libreforge.integrations.LoadableIntegration
 import com.willfp.libreforge.integrations.bettermodel.impl.EffectPlayAnimation
+import com.willfp.libreforge.integrations.bettermodel.impl.EffectStopAnimation
 
 object BetterModelIntegration : LoadableIntegration {
     override fun load(plugin: EcoPlugin) {
         Effects.register(EffectPlayAnimation)
+        Effects.register(EffectStopAnimation)
     }
 
     override fun getPluginName(): String {
--- core/common/src/main/kotlin/com/willfp/libreforge/integrations/bettermodel/impl/EffectStopAnimation.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/integrations/bettermodel/impl/EffectStopAnimation.kt
@@ -4,25 +4,19 @@ import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.arguments
 import com.willfp.libreforge.effects.Effect
-import com.willfp.libreforge.enumValueOfOrNull
 import com.willfp.libreforge.get
 import com.willfp.libreforge.getFormattedString
 import com.willfp.libreforge.triggers.TriggerData
 import kr.toxicity.model.api.BetterModel
-import kr.toxicity.model.api.animation.AnimationIterator
-import kr.toxicity.model.api.animation.AnimationModifier
 import org.bukkit.entity.LivingEntity
 import kotlin.jvm.optionals.getOrNull
 
-object EffectPlayAnimation : Effect<NoCompileData>("play_animation") {
+object EffectStopAnimation : Effect<NoCompileData>("stop_animation") {
     override val isPermanent = false
 
     override val arguments = arguments {
         require("model", "You must specify the animation model!")
         require("animation", "You must specify the animation name!")
-        require("mode", "You must specify the animation mode!", Config::getFormattedString) {
-            enumValueOfOrNull<AnimationIterator.Type>(it.uppercase()) != null
-        }
     }
 
     override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
@@ -30,18 +24,12 @@ object EffectPlayAnimation : Effect<NoCompileData>("play_animation") {
 
         val animationModel = config.getFormattedString("model", data)
         val animationName = config.getFormattedString("animation", data)
-        val animationMode = enumValueOfOrNull<AnimationIterator.Type>(config.getFormattedString("mode", data).uppercase()) ?: return false
 
         val modeledEntity = BetterModel.registry(entity).getOrNull() ?: return false
 
-        val modifier = AnimationModifier.builder()
-            .player(data.player)
-            .type(animationMode)
-            .build()
-
         val tracker = modeledEntity.tracker(animationModel) ?: return false
 
-        tracker.animate(animationName, modifier)
+        tracker.stopAnimation(animationName)
 
         return true
     }
