8533787a2bc65e819d445147048b613a3356833b
NicoNeko <nicuch100@gmail.com>
2026-03-11 15:00:26 +0200

    Fix BetterModel

--- core/common/src/main/kotlin/com/willfp/libreforge/integrations/bettermodel/impl/EffectPlayAnimation.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/integrations/bettermodel/impl/EffectPlayAnimation.kt
@@ -1,5 +1,6 @@
 package com.willfp.libreforge.integrations.bettermodel.impl
 
+import com.nexomc.nexo.utils.applyIf
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.arguments
@@ -8,40 +9,53 @@ import com.willfp.libreforge.enumValueOfOrNull
 import com.willfp.libreforge.get
 import com.willfp.libreforge.getFormattedString
 import com.willfp.libreforge.triggers.TriggerData
+import kr.toxicity.model.api.BetterModel
 import kr.toxicity.model.api.animation.AnimationIterator
 import kr.toxicity.model.api.animation.AnimationModifier
 import kr.toxicity.model.api.bukkit.platform.BukkitAdapter
 import org.bukkit.entity.LivingEntity
-import kotlin.jvm.optionals.getOrNull
 
 object EffectPlayAnimation : Effect<NoCompileData>("play_animation") {
     override val isPermanent = false
 
     override val arguments = arguments {
-        require("model", "You must specify the animation model!")
         require("animation", "You must specify the animation name!")
     }
 
     override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
         val entity = data.dispatcher.get<LivingEntity>() ?: data.victim ?: return false
-        val player = data.player ?: return false
+        val player = data.player
 
-        val animationModel = config.getFormattedString("model", data)
-        val animationName = config.getFormattedString("animation", data)
-        val animationMode =
-            enumValueOfOrNull<AnimationIterator.Type>(config.getFormattedString("mode", data).uppercase())
-                ?: AnimationIterator.Type.PLAY_ONCE
+        val animation = config.getFormattedString("animation", data)
+        val mode =
+            enumValueOfOrNull<AnimationIterator.Type>(
+                config.getFormattedString("mode", data).uppercase()
+            ) ?: AnimationIterator.Type.PLAY_ONCE
 
-        val modeledEntity = BukkitAdapter.adapt(entity)
+        val registry = BetterModel.registryOrNull(entity.uniqueId) ?: return false
 
         val modifier = AnimationModifier.builder()
-            .player(BukkitAdapter.adapt(player))
-            .type(animationMode)
+            .applyIf(config.getBool("self")) {
+                player(player?.let { BukkitAdapter.adapt(player) })
+            }
+            .applyIf(config.has("override")) {
+                override(config.getBool("override"))
+            }
+            .applyIf(config.has("speed")) {
+                speed(config.getDouble("speed").toFloat())
+            }
+            .applyIf(config.has("start")) {
+                start(config.getInt("start"))
+            }
+            .applyIf(config.has("end")) {
+                end(config.getInt("end"))
+            }
+            .type(mode)
             .build()
 
-        val tracker = modeledEntity.tracker(animationModel).getOrNull() ?: return false
-
-        tracker.animate(animationName, modifier)
+        registry.trackers().forEach {
+            it.animate(animation, modifier)
+        }
 
         return true
     }
--- core/common/src/main/kotlin/com/willfp/libreforge/integrations/bettermodel/impl/EffectStopAnimation.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/integrations/bettermodel/impl/EffectStopAnimation.kt
@@ -7,29 +7,25 @@ import com.willfp.libreforge.effects.Effect
 import com.willfp.libreforge.get
 import com.willfp.libreforge.getFormattedString
 import com.willfp.libreforge.triggers.TriggerData
-import kr.toxicity.model.api.bukkit.platform.BukkitAdapter
+import kr.toxicity.model.api.BetterModel
 import org.bukkit.entity.LivingEntity
-import kotlin.jvm.optionals.getOrNull
 
 object EffectStopAnimation : Effect<NoCompileData>("stop_animation") {
     override val isPermanent = false
 
     override val arguments = arguments {
-        require("model", "You must specify the animation model!")
         require("animation", "You must specify the animation name!")
     }
 
     override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
         val entity = data.dispatcher.get<LivingEntity>() ?: data.victim ?: return false
+        val animation = config.getFormattedString("animation", data)
 
-        val animationModel = config.getFormattedString("model", data)
-        val animationName = config.getFormattedString("animation", data)
+        val registry = BetterModel.registryOrNull(entity.uniqueId) ?: return false
 
-        val modeledEntity = BukkitAdapter.adapt(entity)
-
-        val tracker = modeledEntity.tracker(animationModel).getOrNull() ?: return false
-
-        tracker.stopAnimation(animationName)
+        registry.trackers().forEach {
+            it.stopAnimation(animation)
+        }
 
         return true
     }
