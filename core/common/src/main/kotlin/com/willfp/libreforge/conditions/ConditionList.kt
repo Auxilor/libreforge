543d3ce84f4b4f175e8a4d5ee89d36403c12114a
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-03-19 00:00:00 +0000

    Fix formatting of emptyConditionList function documentation in ConditionList

--- core/common/src/main/kotlin/com/willfp/libreforge/conditions/ConditionList.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/conditions/ConditionList.kt
@@ -66,7 +66,8 @@ class ConditionList(
         return lines
     }
 
-/**
- * Create an empty [ConditionList].
- */
-fun emptyConditionList() = ConditionList(emptyList())
+    /**
+     * Create an empty [ConditionList].
+     */
+    fun emptyConditionList() = ConditionList(emptyList())
+}
