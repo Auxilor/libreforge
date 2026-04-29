e30df41f9cab0979c04e3c58b2e05b36a0f9e87c
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2026-04-29 14:35:15 +0100

    Merge pull request #269
    * fix: `from_spawner` filter.

--- core/common/src/main/kotlin/com/willfp/libreforge/filters/impl/FilterFromSpawner.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/filters/impl/FilterFromSpawner.kt
@@ -2,11 +2,15 @@ package com.willfp.libreforge.filters.impl
 
 import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.util.namespacedKeyOf
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.filters.Filter
 import com.willfp.libreforge.triggers.TriggerData
+import org.bukkit.persistence.PersistentDataType
 
 object FilterFromSpawner : Filter<NoCompileData, Boolean>("from_spawner") {
+    private val fromSpawnerKey = namespacedKeyOf("ecomobs", "from_spawner")
+
     override fun getValue(config: Config, data: TriggerData?, key: String): Boolean {
         return config.getBool(key)
     }
@@ -14,10 +18,9 @@ object FilterFromSpawner : Filter<NoCompileData, Boolean>("from_spawner") {
     override fun isMet(data: TriggerData, value: Boolean, compileData: NoCompileData): Boolean {
         val victim = data.victim ?: return true
 
-        if (!Prerequisite.HAS_PAPER.isMet) {
-            return true
-        }
+        val isFromSpawner = victim.persistentDataContainer.has(fromSpawnerKey, PersistentDataType.BYTE)
+            || (Prerequisite.HAS_PAPER.isMet && victim.fromMobSpawner())
 
-        return value == victim.fromMobSpawner()
+        return value == isFromSpawner
     }
 }
