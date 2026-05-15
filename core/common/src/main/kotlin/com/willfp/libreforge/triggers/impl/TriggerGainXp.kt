31f941e7d6194a59a5d0bbe9faa8243b54069904
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-05-15 23:25:49 +0100

    fix: implement telekinesis XP gain handling in TriggerGainXp (#292)

--- core/common/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerGainXp.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerGainXp.kt
@@ -1,11 +1,14 @@
 package com.willfp.libreforge.triggers.impl
 
+import com.willfp.eco.core.events.DropQueuePushEvent
 import com.willfp.eco.core.events.NaturalExpGainEvent
+import com.willfp.libreforge.plugin
 import com.willfp.libreforge.toDispatcher
 import com.willfp.libreforge.triggers.Trigger
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
 import org.bukkit.event.EventHandler
+import java.util.UUID
 
 object TriggerGainXp : Trigger("gain_xp") {
     override val parameters = setOf(
@@ -13,9 +16,15 @@ object TriggerGainXp : Trigger("gain_xp") {
         TriggerParameter.EVENT
     )
 
+    private val telekinesisGranted = mutableSetOf<UUID>()
+
     @EventHandler(ignoreCancelled = true)
     fun handle(event: NaturalExpGainEvent) {
         val player = event.expChangeEvent.player
+
+        if (player.uniqueId in telekinesisGranted) {
+            return
+        }
 
         this.dispatch(
             player.toDispatcher(),
@@ -23,6 +32,27 @@ object TriggerGainXp : Trigger("gain_xp") {
                 player = player,
                 event = event.expChangeEvent,
                 value = event.expChangeEvent.amount.toDouble()
+            )
+        )
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: DropQueuePushEvent) {
+        if (!event.isTelekinetic) return
+        if (event.xp <= 0) return
+
+        val player = event.player
+
+        telekinesisGranted += player.uniqueId
+        plugin.scheduler.run {
+            telekinesisGranted -= player.uniqueId
+        }
+
+        this.dispatch(
+            player.toDispatcher(),
+            TriggerData(
+                player = player,
+                value = event.xp.toDouble()
             )
         )
     }
