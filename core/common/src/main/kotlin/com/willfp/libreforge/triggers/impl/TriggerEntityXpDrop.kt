811477485fbd6e9a63c46302f3cc257c1d1b4558
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-05-30 01:56:51 +0100

    feat: add entity_xp_drop trigger

---
+++ core/common/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerEntityXpDrop.kt
@@ -0,0 +1,58 @@
+package com.willfp.libreforge.triggers.impl
+
+import com.willfp.eco.core.Prerequisite
+import com.willfp.eco.core.events.EntityDeathByEntityEvent
+import com.willfp.libreforge.toDispatcher
+import com.willfp.libreforge.triggers.Trigger
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
+import com.willfp.libreforge.triggers.event.EditableXpDropEvent
+import com.willfp.libreforge.triggers.event.XpDropCause
+import com.willfp.libreforge.triggers.event.XpDropContext
+import com.willfp.libreforge.triggers.tryAsLivingEntity
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+
+object TriggerEntityXpDrop : Trigger("entity_xp_drop") {
+    override val parameters = setOf(
+        TriggerParameter.PLAYER,
+        TriggerParameter.VICTIM,
+        TriggerParameter.EVENT,
+        TriggerParameter.LOCATION
+    )
+
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: EntityDeathByEntityEvent) {
+        if (Prerequisite.HAS_PAPER.isMet) {
+            if (event.deathEvent.isCancelled) {
+                return
+            }
+        }
+
+        val entity = event.victim
+        val killer = event.killer.tryAsLivingEntity() ?: return
+
+        val editableEvent = EditableXpDropEvent(
+            initialXp = event.deathEvent.droppedExp,
+            cause = XpDropCause.ENTITY,
+            context = XpDropContext(
+                player = killer as? Player,
+                entity = entity
+            ),
+            dropLocation = entity.location
+        )
+
+        this.dispatch(
+            killer.toDispatcher(),
+            TriggerData(
+                player = killer as? Player,
+                victim = entity,
+                location = entity.location,
+                event = editableEvent,
+                value = event.deathEvent.droppedExp.toDouble()
+            )
+        )
+
+        event.deathEvent.droppedExp = editableEvent.finalXp
+    }
+}
