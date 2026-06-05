e2766e4ea9e36828421a8a3f4ca7aa8f2db108bf
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2026-06-05 22:19:05 +0100

    fix: MythicMobs API changes

--- core/common/src/main/kotlin/com/willfp/libreforge/integrations/mythicmobs/impl/filter/FilterMythicMobType.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/integrations/mythicmobs/impl/filter/FilterMythicMobType.kt
@@ -5,17 +5,21 @@ import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.filters.Filter
 import com.willfp.libreforge.triggers.TriggerData
 import io.lumine.mythic.bukkit.MythicBukkit
+import io.lumine.mythic.core.mobs.ActiveMob
 
 object FilterMythicMobType : Filter<NoCompileData, Collection<String>>("mythicmob") {
+
     override fun getValue(config: Config, data: TriggerData?, key: String): Collection<String> {
         return config.getStrings(key)
     }
 
     override fun isMet(data: TriggerData, value: Collection<String>, compileData: NoCompileData): Boolean {
         val entity = data.victim ?: return true
-        val instance = MythicBukkit.inst().mobManager.getMythicMobInstance(entity)
-        if (!instance.isPresent) return false
-        val mobType = instance.get().type.internalName
-        return value.any { it.equals(mobType, ignoreCase = true) }
+        val mob: ActiveMob = MythicBukkit.inst().mobManager.getMythicMobInstance(entity) ?: return false
+        val mobType = mob.type.internalName
+        return value.any {
+            it.equals(mobType, ignoreCase = true)
+        }
     }
+
 }
