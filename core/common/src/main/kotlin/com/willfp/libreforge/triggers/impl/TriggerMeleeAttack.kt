2434b504eda0c3ee686f577ca1b741a9a5f8efcb
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-08-28 19:17:51 +0100

    fix: use new paper event for 26.x

--- core/common/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerMeleeAttack.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/triggers/impl/TriggerMeleeAttack.kt
@@ -1,9 +1,12 @@
 package com.willfp.libreforge.triggers.impl
 
+import com.willfp.eco.core.Prerequisite
+import com.willfp.libreforge.plugin
 import com.willfp.libreforge.toDispatcher
 import com.willfp.libreforge.triggers.Trigger
 import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
+import io.papermc.paper.event.player.PrePlayerAttackEntityEvent
 import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
@@ -11,8 +14,21 @@ import org.bukkit.event.EventPriority
 import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.EntityDamageEvent
 import java.util.UUID
+import java.util.concurrent.ConcurrentHashMap
+import com.google.common.base.Function
+import org.bukkit.Bukkit
+import org.bukkit.damage.DamageSource
+import org.bukkit.entity.Entity
+import org.bukkit.event.Listener
 
 object TriggerMeleeAttack : Trigger("melee_attack") {
+    private val dataMap = ConcurrentHashMap<UUID, Float>()
+    init {
+        if (Prerequisite.HAS_PAPER.isMet) {
+            registerPaperExclusiveListeners()
+        }
+    }
+
     override val description = "Fires when the player lands a melee hit on an entity."
 
     override val categories = setOf("combat")
@@ -37,6 +53,41 @@ object TriggerMeleeAttack : Trigger("melee_attack") {
 
     private val processedEvents = mutableSetOf<UUID>()
 
+    fun registerPaperExclusiveListeners() {
+        val mcVersion = Bukkit.getServer().bukkitVersion.split("-").getOrNull(0)?.split(".")?.getOrNull(0)?.toInt() ?: 0
+        if (mcVersion >= 26) {
+            plugin.logger.info("Registering PrePlayerAttackEntityEvent for Minecraft version $mcVersion")
+            plugin.eventManager.registerListener(PaperExclusiveListeners())
+        }
+    }
+
+    private class PaperExclusiveListeners : Listener {
+        @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+        fun handle(event: PrePlayerAttackEntityEvent) {
+            if (!(event.willAttack())) return
+            dataMap[event.player.uniqueId] = event.player.attackCooldown
+            plugin.scheduler.runLater({
+                dataMap.remove(event.player.uniqueId)
+            }, 1L)
+        }
+    }
+
+    class EcoEntityDamageByEntityEvent(
+        damager: Entity,
+        damagee: Entity,
+        cause: DamageCause,
+        damage: Double,
+        val attackCooldown: Float,
+    ) : EntityDamageByEntityEvent(damager, damagee, cause, damage) {
+        constructor(event: EntityDamageByEntityEvent, attackCooldown: Float) : this(
+            damager = event.damager,
+            damagee = event.entity,
+            cause = event.cause,
+            damage = event.damage,
+            attackCooldown = attackCooldown
+        )
+    }
+
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     fun handle(event: EntityDamageByEntityEvent) {
         val attacker = event.damager as? LivingEntity ?: return
@@ -58,7 +109,10 @@ object TriggerMeleeAttack : Trigger("melee_attack") {
                 player = attacker as? Player,
                 victim = victim,
                 location = victim.location,
-                event = event,
+                event = EcoEntityDamageByEntityEvent(
+                    event = event,
+                    attackCooldown = dataMap[event.damager.uniqueId] ?: (attacker as? Player)?.attackCooldown ?: 0f
+                ),
                 item = attacker.equipment?.itemInMainHand,
                 value = event.finalDamage,
                 altValue = event.getDamage(EntityDamageEvent.DamageModifier.BASE)
